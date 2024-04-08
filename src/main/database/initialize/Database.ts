be5059df83efb4d6bda58fec07cbc23363fff916
lvfeng <lvfeng.org@gmail.com>
2024-04-08 13:40:50 +0800

    perf(数据库): sqlite3切换为better-sqlite3

--- src/main/database/initialize/Database.ts
+++ src/main/database/initialize/Database.ts
@@ -8,28 +8,26 @@ import yaml from 'js-yaml'
  */
 export async function createDataTable() {
   // 读取当前数据库的数据表
-  let tables: string[]
-  listAllDataTables().then((tableNames) => {
-    tables = tableNames
-  })
+  let currentTables: string[]
+  listAllDataTables().then(async (tableNames) => {
+    currentTables = tableNames
 
-  // 读取初始化yml
-  const yamlContent = await fs.promises.readFile(
-    './src/main/database/initialize/createDataTables.yml',
-    'utf-8'
-  )
-  const tableNameSqls = yaml.load(yamlContent)
+    // 读取初始化yml
+    const yamlContent = await fs.promises.readFile(
+      './src/main/database/initialize/createDataTables.yml',
+      'utf-8'
+    )
+    const tableNameSqls: { tables: { name: string; sql: string }[] } = yaml.load(yamlContent)
 
-  // 对于当前数据库中不存在的数据表，进行创建
-  tableNameSqls.tables.forEach((tableNameSql: { name: string; sql: string }) => {
-    if (!tables.includes(tableNameSql.name)) {
-      db.serialize(() => {
-        db.run(tableNameSql.sql, (err) => {
-          if (err) {
-            console.error(err.message)
-          }
-        })
-      })
-    }
+    // 对于当前数据库中不存在的数据表，进行创建
+    tableNameSqls.tables.forEach((tableNameSql) => {
+      if (!currentTables.includes(tableNameSql.name)) {
+        try {
+          db.exec(tableNameSql.sql)
+        } catch (e) {
+          console.error(e)
+        }
+      }
+    })
   })
 }
--- src/main/database/util/DatabaseUtil.ts
+++ src/main/database/util/DatabaseUtil.ts
@@ -1,24 +1,20 @@
-import sqlite3 from 'sqlite3'
 import path from 'path'
 import { app } from 'electron'
+import Database from 'better-sqlite3'
 
-let DB_PATH = path.join(app.getAppPath(), '/database/pre-work.db')
+let DB_PATH = path.join(app.getAppPath(), '/database/CollectionsManager.db')
 const NODE_ENV = process.env.NODE_ENV
 if (NODE_ENV !== 'development') {
-  DB_PATH = path.join(path.dirname(app.getPath('exe')), '/database/pre-work.db')
+  DB_PATH = path.join(path.dirname(app.getPath('exe')), '/database/CollectionsManager.db')
 }
 
 /**
  * @Description: 连接数据库
  * @CreationDate 2023-05-10 13:48:41
  */
-function connectDatabase(): sqlite3.Database {
-  return new sqlite3.Database(DB_PATH, (err) => {
-    if (err) {
-      console.error('connectDatabaseErr: ' + err.message)
-    }
-    console.log('sqlite3 connected')
-  })
+function connectDatabase() {
+  const options = {}
+  return new Database(DB_PATH, options)
 }
 
 export const db = connectDatabase()
@@ -28,17 +24,12 @@ export const db = connectDatabase()
  */
 export function listAllDataTables(): Promise<string[]> {
   return new Promise((resolve, reject) => {
-    db.serialize(() => {
-      db.all<{ name: string }>(
-        "select name from sqlite_master where name != 'sqlite_sequence'",
-        (err, rows) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(rows.map((row) => row.name))
-          }
-        }
-      )
-    })
+    try {
+      const statement = db.prepare("select name from sqlite_master where name != 'sqlite_sequence'")
+      const rows = statement.all()
+      resolve(rows.map((row) => row.name))
+    } catch (e) {
+      reject(e)
+    }
   })
 }
