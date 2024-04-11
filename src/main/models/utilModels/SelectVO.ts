6cfef2584553cae5964951e49b740d4f6df1e8be
lvfeng <1484891945@qq.com>
2024-04-11 23:48:57 +0800

    fix(主进程): SelectVO新增extraData字段

--- src/main/models/utilModels/SelectVO.ts
+++ src/main/models/utilModels/SelectVO.ts
@@ -5,9 +5,11 @@ export default class SelectVO {
   value: string | null | undefined
   label: string | null | undefined
   rootId: string | null | undefined
+  extraData: object | null | undefined
   constructor(selectVO: SelectVO) {
     this.value = selectVO.value
     this.label = selectVO.label
     this.rootId = selectVO.rootId
+    this.extraData = selectVO.extraData
   }
 }
