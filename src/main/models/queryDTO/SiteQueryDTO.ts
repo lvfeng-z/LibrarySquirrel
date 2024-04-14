806cf40611417a2349adb724ee5283b83f303b77
lvfeng <1484891945@qq.com>
2024-04-14 22:58:38 +0800

    perf: 引入queryDTO

---
+++ src/main/models/queryDTO/SiteQueryDTO.ts
@@ -0,0 +1,32 @@
+/**
+ * QueryDTO
+ * 站点标签
+ */
+import { BaseQueryDTO } from './BaseQueryDTO'
+
+export default class SiteQueryDTO extends BaseQueryDTO {
+  /**
+   * 主键
+   */
+  id: string | undefined | null
+  /**
+   * 站点名称
+   */
+  siteName: string | undefined | null
+  /**
+   * 站点域名
+   */
+  siteDomain: string | undefined | null
+  /**
+   * 站点主页
+   */
+  siteHomepage: string | undefined | null
+
+  constructor(siteQueryDTO: SiteQueryDTO) {
+    super(siteQueryDTO)
+    this.id = siteQueryDTO.id
+    this.siteName = siteQueryDTO.siteName
+    this.siteDomain = siteQueryDTO.siteDomain
+    this.siteHomepage = siteQueryDTO.siteHomepage
+  }
+}
