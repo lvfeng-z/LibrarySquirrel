6ebd086a37bccf5365b8df4c7bde611cf7fe0911
zouwentao <T7.28@mecp>
2024-04-19 10:03:54 +0800

    fix: 调整SideMenuHorizontal样式

--- src/renderer/src/components/SideMenuHorizontal.vue
+++ src/renderer/src/components/SideMenuHorizontal.vue
@@ -1,14 +1,3 @@
-<template>
-  <div class="side-menu">
-    <el-menu class="side-menu-main" :collapse="isCollapsed">
-      <div class="aside-control-button" @click.once="toggleAside">
-        <label class="el-icon-caret-left">{{ isCollapsed ? '>' : '<' }}</label>
-      </div>
-      <slot name="default"></slot>
-    </el-menu>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { ref } from 'vue'
 
@@ -16,43 +5,83 @@ import { ref } from 'vue'
 const isCollapsed = ref(true)
 
 // 方法
-function toggleAside() {
-  isCollapsed.value = !isCollapsed.value
+function toggleAside(status: boolean) {
+  isCollapsed.value = status
 
   // 如果需要在其他地方同步更新菜单状态，可以考虑使用事件总线（EventBus）、Vuex状态管理或组件通信方式
 }
 </script>
 
+<template>
+  <div class="side-menu">
+    <el-menu class="side-menu-main" :collapse="isCollapsed">
+      <div class="side-menu-button-wrapper">
+        <div class="side-menu-button-wrapper-upper">
+          <div class="side-menu-button-upper side-menu-button" @click.once="toggleAside(false)"></div>
+        </div>
+        <div class="side-menu-button-wrapper-lower">
+          <div class="side-menu-button-lower side-menu-button" @click.once="toggleAside(true)"></div>
+        </div>
+      </div>
+      <slot name="default"></slot>
+    </el-menu>
+  </div>
+</template>
+
 <style scoped>
-.aside-control-button {
-  position: absolute;
-  width: 25px;
-  height: 80px;
-  background-color: rgba(232, 232, 232, 0.53);
-  right: -23px;
-  top: 45%;
-  /*perspective：对元素进行透视操作*/
-  /*rotateX：以y轴（横轴）进行旋转（前后仰俯）
-  画个梯形出来*/
-  transform: perspective(2em) rotateY(58deg);
-  text-align: center;
-  line-height: 80px;
-  color: #909399;
-  background-color: #a6cc9d;
-}
-.aside-control-button:hover {
-  background-color: #d0ffc5;
-}
 .side-menu {
   height: 100%;
 }
 .side-menu-main {
-  width: 65px;
+  width: 63px;
   height: 100%;
   min-height: calc(88.3vh);
 }
 .side-menu-main:not(.el-menu--collapse) {
   width: 200px;
   height: 100%;
+}
+.side-menu-button-wrapper {
+  position: absolute;
+  width: 20px;
+  height: 102px;
+  right: -20px;
+  top: calc(50% - 51px);
+}
+.side-menu-button-wrapper-upper {
+  width: 20px;
+  height: 51px;
+  right: -20px;
+  top: 45%;
+  overflow: hidden; /* 隐藏超出边界的部分 */
+  direction: rtl;
+  border-bottom: 1px solid #eeeeee;
+}
+.side-menu-button-upper {
+  width: 40px;
+  height: 100px;
+  border-radius: 100% / 100%;
+  background-color: darkgrey;
+}
+.side-menu-button-wrapper-lower {
+  width: 20px;
+  height: 51px;
+  right: -20px;
+  top: 45%;
+  overflow: hidden; /* 隐藏超出边界的部分 */
+  display: grid;
+  align-content: end;
+  justify-content: start;
+  direction: rtl;
+  border-top: 1px solid #eeeeee;
+}
+.side-menu-button-lower {
+  width: 40px;
+  height: 100px;
+  border-radius: 100% / 100%;
+  background-color: darkgrey;
+}
+.side-menu-button:hover {
+  background-color: lavender;
 }
 </style>
