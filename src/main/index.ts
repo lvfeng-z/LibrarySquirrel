222bd49c2c5c3f8656e5627edb53428b19d3c095
lvfeng <lvfeng.org@gmail.com>
2024-04-20 13:45:38 +0800

    fix: 调整默认窗口比例

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,8 +10,8 @@ import logUtil from './util/LogUtil'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 800,
+    width: 1600,
+    height: 1000,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
