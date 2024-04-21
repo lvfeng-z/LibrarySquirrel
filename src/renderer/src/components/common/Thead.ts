471575c5401652ae852491dc1779a102aef68f0b
lvfeng <lvfeng.org@gmail.com>
2024-04-22 00:16:53 +0800

    perf: Thead表头接口从DataTable中提取出来

---
+++ src/renderer/src/components/common/Thead.ts
@@ -0,0 +1,12 @@
+// DataTable的表头
+export interface Thead {
+  name: string // 字段名
+  label: string // 标题名称
+  dataType: string // 数据类型
+  hide: boolean // 是否隐藏
+  width?: number // 数据列宽度
+  headerAlign?: 'center' | 'left' | 'right' // 标题停靠位置
+  headerTagType?: 'warning' | 'info' | 'success' | 'primary' | 'danger' // 标题使用的el-tag样式
+  dataAlign?: 'center' | 'left' | 'right' // 数据停靠位置
+  overHide?: boolean //列超出长度时是否省略
+}
