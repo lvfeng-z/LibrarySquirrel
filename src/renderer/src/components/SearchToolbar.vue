e7d8fdd895abbaa045d2d38057a428d4031d5494
lvfeng <lvfeng.org@gmail.com>
2024-04-21 18:10:49 +0800

    fix: SearchToolbar、DropdownTable样式调整

--- src/renderer/src/components/SearchToolbar.vue
+++ src/renderer/src/components/SearchToolbar.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-// 类型
-// 搜索项
 import { onBeforeMount, Ref, ref, UnwrapRef } from 'vue'
 import { SearchBox } from './common/SearchBox'
 import DropdownTable from './DropdownTable.vue'
+import ScrollTextBox from './ScrollTextBox.vue'
 
 // props
 const props = defineProps<{
@@ -21,33 +20,31 @@ const searchButtonSpan = ref(3) // 查询按钮的span
 const innerMainSearchBoxes: Ref<UnwrapRef<SearchBox[]>> = ref([]) // 主搜索栏中元素的列表
 const innerDropDownSearchBoxes: Ref<UnwrapRef<SearchBox[]>> = ref([]) // 下拉搜索框中元素的列表
 const showDropdownFlag: Ref<UnwrapRef<boolean>> = ref(false) // 暂时废弃
+const formData = ref({})
 
 // 方法
 // 处理主搜索栏和下拉搜索框
 function calculateSpan() {
   let spanRest = 24 - searchButtonSpan.value
   for (const searchBox of props.mainSearchBoxes) {
+    // 储存当前box的长度
+    let boxSpan = 0
     // 不更改props属性
-    const tempSearchBox = JSON.parse(JSON.stringify(searchBox))
+    const tempSearchBox: SearchBox = JSON.parse(JSON.stringify(searchBox))
 
     if (spanRest > 0) {
-      // 先减去输入框的长度
-      if (tempSearchBox.inputSpan == undefined) {
-        tempSearchBox.inputSpan = 5
-        spanRest -= tempSearchBox.inputSpan
-      } else {
-        spanRest -= tempSearchBox.inputSpan
-      }
-
-      // 再减去已定义的字段名长度
+      // 未设置tag长度则设置为2
       if (tempSearchBox.tagSpan == undefined) {
         tempSearchBox.tagSpan = 2
-        spanRest -= tempSearchBox.tagSpan
-      } else {
-        spanRest -= tempSearchBox.tagSpan
+      }
+      // 未设置input长度则设置为5
+      if (tempSearchBox.inputSpan == undefined) {
+        tempSearchBox.inputSpan = 5
       }
 
-      // 空间不足就放进dropDownSearchBoxes
+      // 判断是否能够容纳此box，空间不足就放进dropDownSearchBoxes
+      boxSpan += tempSearchBox.tagSpan + tempSearchBox.inputSpan
+      spanRest -= boxSpan
       if (spanRest < 0) {
         innerDropDownSearchBoxes.value.push(tempSearchBox)
       } else {
@@ -73,19 +70,39 @@ function calculateSpan() {
 function showDropdown() {
   showDropdownFlag.value = !showDropdownFlag.value
 }
+
+// //
+// function handleGetParams() {
+//   emits('paramsChanged')
+// }
+//
+// // 事件
+// const emits = defineEmits(['getParams'])
+// test
+function test() {
+  console.log(formData.value)
+}
 </script>
 
 <template>
   <div class="search-toolbar">
-    <div class="search-toolbar-wrapper">
-      <div class="search-toolbar-main">
+    <div class="search-toolbar-main" style="">
+      <el-form v-model="formData" @input="test">
         <el-row class="search-toolbar-main-row">
           <template v-for="(item, index) in innerMainSearchBoxes" :key="index">
-            <el-col :span="item.tagSpan">
-              <el-tag :key="index" size="large">{{ item.label }}</el-tag>
+            <el-col class="search-toolbar-label" :span="item.tagSpan">
+              <div
+                :key="index"
+                class="search-toolbar-label-scroll-text-wrapper el-tag-mimic"
+                style="width: 100%"
+              >
+                <ScrollTextBox>{{ item.label }}</ScrollTextBox>
+              </div>
             </el-col>
-            <el-col :span="item.inputSpan">
-              <el-input></el-input>
+            <el-col class="search-toolbar-input" :span="item.inputSpan">
+              <el-form-item>
+                <el-input v-model="formData[item.name]"></el-input>
+              </el-form-item>
             </el-col>
           </template>
           <el-col
@@ -103,11 +120,12 @@ function showDropdown() {
             </el-dropdown>
           </el-col>
         </el-row>
-      </div>
+      </el-form>
     </div>
+    <!-- 为了不被el-table内置的2层z轴遮挡，此处为2层z轴 -->
     <div class="dropdown-menu-wrapper z-layer-2">
-      <div class="dropdown-menu">
-        <DropdownTable :search-boxes="[]"></DropdownTable>
+      <div class="dropdown-menu rounded-borders">
+        <DropdownTable :search-boxes="innerDropDownSearchBoxes"></DropdownTable>
       </div>
     </div>
   </div>
@@ -119,22 +137,38 @@ function showDropdown() {
   height: calc(32px + 10px);
   display: flex;
   flex-direction: column;
+  align-content: center;
 }
-
 .search-toolbar-main-row {
+  height: 32px;
   width: 100%;
 }
-
 .search-toolbar-search-button {
   display: flex;
   justify-content: flex-end;
 }
+.search-toolbar-label {
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+}
+.search-toolbar-input {
+  display: flex;
+  justify-content: flex-start;
+}
 .dropdown-menu-wrapper {
-  height: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
 }
 .dropdown-menu {
   width: 100%;
   height: auto;
+  margin-top: 1px;
+  background-color: white;
+  background-clip: padding-box;
+  box-shadow: inset 0 0 0 1.5px rgba(0, 0, 0, 0); /* 利用内阴影模拟边框外背景透明效果 */
 }
 </style>
