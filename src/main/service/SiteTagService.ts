0f1960c41b447effd2a5ac7077e7c7f44c64c4b6
lvfeng <lvfeng.org@gmail.com>
2024-04-26 22:52:52 +0800

    fix(主进程-SiteTagService.ts): 返回值使用ApiUtil包装

--- src/main/service/SiteTagService.ts
+++ src/main/service/SiteTagService.ts
@@ -2,6 +2,8 @@ import SiteTag from '../models/SiteTag'
 import SiteTagDao from '../dao/SiteTagDao'
 import SiteTagQueryDTO from '../models/queryDTO/SiteTagQueryDTO'
 import { SiteTagDaoTest } from '../dao/SiteTagDaoTest'
+import { ApiUtil } from '../util/ApiUtil'
+import LogUtil from '../util/LogUtil'
 
 async function save(siteTag: SiteTag) {
   const dao = new SiteTagDaoTest()
@@ -9,9 +11,13 @@ async function save(siteTag: SiteTag) {
 }
 
 async function updateById(siteTag: SiteTag) {
-  console.log('SiteTagService.ts.updateById', siteTag)
   const dao = new SiteTagDaoTest()
-  return await dao.updateById(siteTag.id, siteTag)
+  if (siteTag.id != undefined) {
+    return ApiUtil.check((await dao.updateById(siteTag.id, siteTag)) > 0)
+  } else {
+    LogUtil.error('SiteTagService.ts', 'updateById更新时id意外为空')
+    return ApiUtil.error('')
+  }
 }
 
 function getSelectList(queryDTO: SiteTagQueryDTO) {
