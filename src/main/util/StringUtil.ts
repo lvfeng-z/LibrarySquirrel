73b87aaf7f4bb2403799eab4eb270492cf932e2f
zouwentao <T7.28@mecp>
2024-04-26 17:37:38 +0800

    feat(主进程-StringUtil.ts): 驼峰和蛇形互相转换工具

--- src/main/util/StringUtil.ts
+++ src/main/util/StringUtil.ts
@@ -6,7 +6,16 @@ function isNotBlank(input: string) {
   return !isBlank(input)
 }
 
+function camelToSnakeCase(str: string): string {
+  return str.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()
+}
+
+function snakeToCamelCase(str: string): string {
+  return str.replace(/(_\w)/g, (m) => m[1].toUpperCase())
+}
 export default {
   isBlank,
-  isNotBlank
+  isNotBlank,
+  camelToSnakeCase,
+  snakeToCamelCase
 }
