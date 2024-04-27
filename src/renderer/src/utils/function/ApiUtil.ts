55b39f859ed8bee4d63907e38fc071e6c45bc0fd
lvfeng <lvfeng.org@gmail.com>
2024-04-27 21:39:03 +0800

    fix(渲染进程-ApiUtil.ts): parseApiResponse改为apiResponseNotice，并专注于消息提醒，不再处理返回

--- src/renderer/src/utils/function/ApiUtil.ts
+++ src/renderer/src/utils/function/ApiUtil.ts
@@ -1,19 +1,19 @@
 import { ElMessage } from 'element-plus'
 
-export function parseApiResponse(
-  response: { success: boolean; msg: string; data: unknown },
-  showMsg: boolean
-) {
+export function apiResponseNotice(response: { success: boolean; msg: string; data: unknown }): void {
   if (response?.success) {
-    return response.data
+    if(response?.msg) {
+      ElMessage({
+        message: response?.msg ?? '操作成功',
+        type: 'success'
+      })
+    }
+  } else {
+    if (response?.msg) {
+      ElMessage({
+        message: response?.msg ?? '未知错误',
+        type: 'error'
+      })
+    }
   }
-
-  if (showMsg && (response?.msg || '未知错误')) {
-    ElMessage({
-      message: response?.msg ?? '未知错误',
-      type: 'error'
-    })
-  }
-
-  return undefined
 }
