b12cbaf83a83ba980ffb284ec26c99b8f9bb1236
lvfeng <lvfeng.org@gmail.com>
2024-04-27 22:03:58 +0800

    fix(渲染进程-ApiUtil.ts): apiResponseNotice改为apiResponseMsg，新增apiResponseMsgNoSuccess

--- src/renderer/src/utils/function/ApiUtil.ts
+++ src/renderer/src/utils/function/ApiUtil.ts
@@ -1,11 +1,34 @@
 import { ElMessage } from 'element-plus'
 
-export function apiResponseNotice(response: { success: boolean; msg: string; data: unknown }): void {
+export function apiResponseMsg(response: { success: boolean; msg: string; data: unknown }): void {
   if (response?.success) {
-    if(response?.msg) {
+    if (response?.msg) {
       ElMessage({
-        message: response?.msg ?? '操作成功',
-        type: 'success'
+        message: response?.msg ?? null,
+        type: 'success',
+        icon: 'SuccessFilled'
+      })
+    }
+  } else {
+    if (response?.msg) {
+      ElMessage({
+        message: response?.msg ?? '未知错误',
+        type: 'error'
+      })
+    }
+  }
+}
+
+export function apiResponseMsgNoSuccess(response: {
+  success: boolean
+  msg: string
+  data: unknown
+}): void {
+  if (response?.success) {
+    if (response?.msg) {
+      ElMessage({
+        type: 'success',
+        duration: 1000
       })
     }
   } else {
