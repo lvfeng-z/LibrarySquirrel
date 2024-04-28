2d40bfc641b9195747d46e7d52f913940c5fbc51
lvfeng <lvfeng.org@gmail.com>
2024-04-29 01:21:28 +0800

    fix(主进程-SiteTagQueryDTO.ts): SiteTagQueryDTO的所有属性调整为可选

--- src/main/model/queryDTO/SiteTagQueryDTO.ts
+++ src/main/model/queryDTO/SiteTagQueryDTO.ts
@@ -8,37 +8,42 @@ export default class SiteTagQueryDTO extends BaseQueryDTO {
   /**
    * 主键
    */
-  id: number | undefined | null
+  id?: number | undefined | null
   /**
    * 标签来源站点id
    */
-  siteId: number | undefined | null
+  siteId?: number | undefined | null
   /**
    * 站点中标签的id
    */
-  siteTagId: string | undefined | null
+  siteTagId?: string | undefined | null
   /**
    * 站点中标签的名称
    */
-  siteTagName: string | undefined | null
+  siteTagName?: string | undefined | null
   /**
    * 上级标签id
    */
-  baseSiteTagId: string | undefined | null
+  baseSiteTagId?: string | undefined | null
   /**
    * 描述
    */
-  description: string | undefined | null
+  description?: string | undefined | null
   /**
    * 站点标签对应的本地标签id
    */
-  localTagId: number | undefined | null
+  localTagId?: number | undefined | null
 
   // 查询用字段
   /**
    * 标签来源站点id列表
    */
-  sites: string[] | undefined | null
+  sites?: string[] | undefined | null
+
+  /**
+   * 查询绑定在localTagId上的，还是未绑定的（true：绑定的，false：未绑定的）
+   */
+  bound?: boolean | undefined | null
 
   constructor(siteTagQueryDTO: SiteTagQueryDTO) {
     super(siteTagQueryDTO)
@@ -50,5 +55,6 @@ export default class SiteTagQueryDTO extends BaseQueryDTO {
     this.description = siteTagQueryDTO.description
     this.localTagId = siteTagQueryDTO.localTagId
     this.sites = siteTagQueryDTO.sites
+    this.bound = siteTagQueryDTO.bound
   }
 }
