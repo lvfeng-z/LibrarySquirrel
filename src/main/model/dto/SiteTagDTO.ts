19289a0cfdfad3d63b82abbdc016facbb6921e71
zouwentao <T7.28@mecp>
2024-04-29 14:04:49 +0800

    fix(主进程-SiteTagDTO.ts): SiteTagDTO引入site实例属性

--- src/main/model/dto/SiteTagDTO.ts
+++ src/main/model/dto/SiteTagDTO.ts
@@ -1,8 +1,17 @@
 import SiteTag from '../SiteTag'
 import LocalTag from '../LocalTag'
+import Site from '../Site'
 
 export class SiteTagDTO extends SiteTag {
+  /**
+   * 绑定的本地标签的实例
+   */
   localTag: LocalTag
+
+  /**
+   * 来源站点的实例
+   */
+  site: Site
 
   constructor(siteTagDTO: SiteTagDTO) {
     super(siteTagDTO)
@@ -10,6 +19,11 @@ export class SiteTagDTO extends SiteTag {
       this.localTag = JSON.parse(siteTagDTO.localTag)
     } else {
       this.localTag = siteTagDTO.localTag
+    }
+    if (typeof siteTagDTO.site == 'string') {
+      this.site = JSON.parse(siteTagDTO.site)
+    } else {
+      this.site = siteTagDTO.site
     }
   }
 }
