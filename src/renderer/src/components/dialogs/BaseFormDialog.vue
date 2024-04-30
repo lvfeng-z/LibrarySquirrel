bc1c7d26ce0b24a86eea7305cf5dd77fa3549d55
捋风 <lvfeng.org@gmail.com>
2024-05-01 00:11:52 +0800

    feat(渲染进程-BaseFormDialog.vue、LocalTagDialog.vue): BaseFormDialog和LocalTagDialog初步创建

---
+++ src/renderer/src/components/dialogs/BaseFormDialog.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <el-dialog>
+    <el-form>
+      <slot name="default"></slot>
+    </el-form>
+  </el-dialog>
+</template>
+
+<style scoped></style>
---
+++ src/renderer/src/components/dialogs/LocalTagDialog.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import BaseFormDialog from './BaseFormDialog.vue'
+</script>
+
+<template>
+  <BaseFormDialog>
+    <template #default>
+      <el-row></el-row>
+    </template>
+  </BaseFormDialog>
+</template>
+
+<style scoped></style>
