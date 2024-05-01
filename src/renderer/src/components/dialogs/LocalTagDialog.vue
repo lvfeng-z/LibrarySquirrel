de1b99aa89280db2fa67c0202a6441a03801ef68
捋风 <lvfeng.org@gmail.com>
2024-05-01 22:36:40 +0800

    feat(渲染进程-BaseFormDialog.vue、LocalTagDialog.vue): BaseFormDialog、LocalTagDialog持续推进

--- src/renderer/src/components/dialogs/LocalTagDialog.vue
+++ src/renderer/src/components/dialogs/LocalTagDialog.vue
@@ -3,11 +3,7 @@ import BaseFormDialog from '../common/BaseFormDialog.vue'
 </script>
 
 <template>
-  <BaseFormDialog :input-boxes="[]">
-    <template #default>
-      <el-row></el-row>
-    </template>
-  </BaseFormDialog>
+  <BaseFormDialog :input-boxes="[]"> </BaseFormDialog>
 </template>
 
 <style scoped></style>
