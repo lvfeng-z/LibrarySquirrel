dea34e1613ea5b71e0aa20fe2c9989c71ed752ac
捋风 <lvfeng.org@gmail.com>
2024-05-02 00:08:44 +0800

    fix(渲染进程-BaseCloseablePage.vue、App.vue): 调整BaseCloseablePage关闭事件名称，新增dialog插槽

--- src/renderer/src/components/closeablePage/BaseCloseablePage.vue
+++ src/renderer/src/components/closeablePage/BaseCloseablePage.vue
@@ -1,48 +1,48 @@
 <script setup lang="ts">
 // 事件
-const emits = defineEmits(['close-float-page'])
+const emits = defineEmits(['closeSelf'])
 
 // 方法
 function closeSelf() {
-  emits('close-float-page')
+  emits('closeSelf')
 }
 </script>
 
 <template>
-  <div class="BaseCloseablePage">
+  <div class="base-closeable-page">
     <el-button
-      class="close-button"
+      class="base-closeable-page-close-button"
       circle
       icon="close"
       color="#b54747"
       @click="closeSelf"
     ></el-button>
-    <div class="content">
+    <div class="base-closeable-page-content">
       <slot />
+    </div>
+    <div class="base-closeable-page-dialog">
+      <slot name="dialog" />
     </div>
   </div>
 </template>
 
 <style>
-.BaseCloseablePage {
+.base-closeable-page {
   display: flex;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
   width: 100%;
   height: 100%;
   background: white;
   justify-content: center;
   align-items: center;
 }
-
-.content {
+.base-closeable-page-dialog {
+  position: absolute;
+}
+.base-closeable-page-content {
   height: calc(100% - 90px);
   width: calc(100% - 90px);
 }
-
-.close-button {
+.base-closeable-page-close-button {
   position: absolute;
   top: 16px;
   right: 16px;
