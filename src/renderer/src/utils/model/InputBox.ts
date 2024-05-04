c6c4c50802f81ab849386c20f15eca9a1cf54830
lvfeng <lvfeng.org@gmail.com>
2024-05-04 18:25:54 +0800

    fix(渲染进程-InputBox.ts): 调整InputBox属性

--- src/renderer/src/utils/model/InputBox.ts
+++ src/renderer/src/utils/model/InputBox.ts
@@ -1,9 +1,10 @@
 export interface InputBox {
   name: string // 字段名
-  inputType: string // 输入组件类型
-  dataType: string // 数据类型
   placeholder: string // 占位符
+  show?: boolean // 是否展示本InputBox
+  inputType?: 'text' | 'date' | 'dateTime' | 'number' | 'textarea' // 输入组件类型
   inputSpan?: number // 输入组件宽度
+  disabled?: boolean // 是否禁用
   label?: string // 标题名称
   labelSpan?: number // 标题宽度
   showLabel?: boolean // 是否展示标题
