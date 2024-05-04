6168ee94cf5447cbada14836f26684a534c84247
lvfeng <lvfeng.org@gmail.com>
2024-05-05 00:31:58 +0800

    fix(渲染进程-BaseFormDialog.vue): 调整BaseFormDialog样式

--- src/renderer/src/components/common/BaseFormDialog.vue
+++ src/renderer/src/components/common/BaseFormDialog.vue
@@ -1,36 +1,38 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+// props
+const props = defineProps<{
+  initialFormData: object
+}>()
+
 // 事件
-const emits = defineEmits(['paramsChanged'])
+const emits = defineEmits(['formDataChanged'])
 
 // 变量
-const formData = ref({})
+const formData = ref(props.initialFormData)
 
 // 方法
-// 用户输入改变时，发送paramsChanged事件
-function handleParamsChanged() {
-  emits('paramsChanged', formData.value)
+// 用户输入改变时，发送formDataChanged事件
+function handleFormDataChanged() {
+  emits('formDataChanged', formData.value)
 }
 </script>
 
 <template>
   <el-dialog class="dialog-form">
-    <el-scrollbar class="dialog-form-rows">
-      <el-form v-model="formData" @input="handleParamsChanged">
-        <slot name="default" />
-      </el-form>
+    <el-scrollbar class="dialog-form-scroll">
+      <div class="dialog-form-scroll-form">
+        <el-form v-model="formData" @input="handleFormDataChanged">
+          <slot name="default" />
+        </el-form>
+      </div>
     </el-scrollbar>
   </el-dialog>
 </template>
 
 <style scoped>
-.dialog-form {
-  width: 100%;
-  position: relative;
-}
-.dialog-form-rows {
-  flex-direction: column;
-  width: 100%;
+.dialog-form-scroll-form {
+  max-height: 90vh;
 }
 </style>
