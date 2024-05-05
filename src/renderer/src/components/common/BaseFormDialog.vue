92f64d75db9d256ea76b55b296149b6060526123
lvfeng <lvfeng.org@gmail.com>
2024-05-05 23:03:32 +0800

    fix(渲染进程-BaseFormDialog.vue、SimpleFormDialog.vue): 控制模式的逻辑和下方按钮移动至BaseFormDialog

--- src/renderer/src/components/common/BaseFormDialog.vue
+++ src/renderer/src/components/common/BaseFormDialog.vue
@@ -1,31 +1,80 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import { DialogMode } from '../../model/util/DialogMode'
 
 // props
 const props = defineProps<{
-  initialFormData: object
+  mode: DialogMode
+  header?: string // 标题
 }>()
 
+// model
+// 表单数据
+const formData = defineModel<object | undefined>('formData', {
+  default: () => undefined
+})
+// 弹窗开关
+const state = defineModel<boolean>('state', {
+  default: () => false
+})
+
+// onMounted
+onMounted(() => {
+  switch (props.mode) {
+    case DialogMode.VIEW:
+      saveButtonState.value = false
+      formDisabled.value = true
+      break
+    case DialogMode.EDIT:
+      saveButtonState.value = true
+      formDisabled.value = false
+      break
+    case DialogMode.NEW:
+      saveButtonState.value = true
+      formDisabled.value = false
+      break
+    default:
+  }
+})
+
 // 事件
-const emits = defineEmits(['formDataChanged'])
+const emits = defineEmits(['saveButtonClicked', 'cancelButtonClicked'])
 
 // 变量
-const formData = ref(props.initialFormData)
+// 表单总开关
+const formDisabled = ref(true)
+// 保存按钮开关
+const saveButtonState = ref(false)
 
 // 方法
-// 用户输入改变时，发送formDataChanged事件
-function handleFormDataChanged() {
-  emits('formDataChanged', formData.value)
+// 处理保存按钮点击事件
+function handleSaveButtonClicked() {
+  emits('saveButtonClicked')
+}
+// 处理保存按钮点击事件
+function handleCancelButtonClicked() {
+  emits('cancelButtonClicked')
 }
 </script>
 
 <template>
-  <el-dialog class="dialog-form">
+  <el-dialog v-model="state" class="dialog-form" center>
+    <template v-if="props.header" #header>
+      <h4>{{ props.header }}</h4>
+    </template>
     <el-scrollbar class="dialog-form-scroll">
       <div class="dialog-form-scroll-form">
-        <el-form v-model="formData" @input="handleFormDataChanged">
+        <el-form v-model="formData" :disabled="formDisabled">
           <slot name="default" />
         </el-form>
+        <el-row>
+          <el-col v-show="saveButtonState" :span="3">
+            <el-button type="primary" @click="handleSaveButtonClicked">保存</el-button>
+          </el-col>
+          <el-col :span="3">
+            <el-button @click="handleCancelButtonClicked">取消</el-button>
+          </el-col>
+        </el-row>
       </div>
     </el-scrollbar>
   </el-dialog>
