9ebcb3e38a579b6789193554b586b230b9a77e4a
lvfeng <lvfeng.org@gmail.com>
2024-05-06 16:11:35 +0800

    feat(主进程-BaseDao.ts、PageModel.ts): BaseDao和PageModel的泛型将查询和结果的泛型区分开

--- src/main/dao/BaseDao.ts
+++ src/main/dao/BaseDao.ts
@@ -6,16 +6,18 @@ import BaseModel from '../model/BaseModel'
 type PrimaryKey = string | number
 
 // 基础数据操作接口
-export interface BaseDao<T extends BaseModel> {
-  save(entity: T): Promise<number | string>
-  updateById(id: PrimaryKey, updateData: Partial<T>): Promise<number>
+export interface BaseDao<Query extends BaseModel, Result> {
+  save(entity: Query): Promise<number | string>
   deleteById(id: PrimaryKey): Promise<number>
-  selectPage(page: PageModel<T>): Promise<PageModel<T>>
-  getById(id: PrimaryKey): Promise<T>
+  updateById(id: PrimaryKey, updateData: Partial<Query>): Promise<number>
+  getById(id: PrimaryKey): Promise<Query>
+  selectPage(page: PageModel<Query, Result>): Promise<PageModel<Query, Result>>
 }
 
 // 抽象基类，实现基本的CRUD方法
-export abstract class AbstractBaseDao<T extends BaseModel> implements BaseDao<T> {
+export abstract class AbstractBaseDao<Query extends BaseModel, Result>
+  implements BaseDao<Query, Result>
+{
   protected tableName: string
   private readonly childClassName: string
 
@@ -24,7 +26,7 @@ export abstract class AbstractBaseDao<T extends BaseModel> implements BaseDao<T>
     this.childClassName = childClassName
   }
 
-  async save(entity: Partial<T>): Promise<number | string> {
+  async save(entity: Partial<Query>): Promise<number | string> {
     const db = this.acquire()
     try {
       // 设置createTime和updateTime
@@ -40,7 +42,17 @@ export abstract class AbstractBaseDao<T extends BaseModel> implements BaseDao<T>
     }
   }
 
-  async updateById(id: PrimaryKey, updateData: Partial<T>): Promise<number> {
+  async deleteById(id: PrimaryKey): Promise<number> {
+    const db = this.acquire()
+    try {
+      const sql = `DELETE FROM "${this.tableName}" WHERE "${this.getPrimaryKeyColumnName()}" = ${id}`
+      return (await db.prepare(sql)).run().changes
+    } finally {
+      db.release()
+    }
+  }
+
+  async updateById(id: PrimaryKey, updateData: Partial<Query>): Promise<number> {
     const db = this.acquire()
     try {
       // 设置createTime和updateTime
@@ -55,49 +67,7 @@ export abstract class AbstractBaseDao<T extends BaseModel> implements BaseDao<T>
     }
   }
 
-  async deleteById(id: PrimaryKey): Promise<number> {
-    const db = this.acquire()
-    try {
-      const sql = `DELETE FROM "${this.tableName}" WHERE "${this.getPrimaryKeyColumnName()}" = ${id}`
-      return (await db.prepare(sql)).run().changes
-    } finally {
-      db.release()
-    }
-  }
-
-  async selectPage(page: PageModel<T>): Promise<PageModel<T>> {
-    const db = this.acquire()
-    try {
-      let whereClause = ''
-      if (page.query) {
-        const conditions = Object.entries(page.query)
-          .map(([key, value]) => `"${key}" = '${String(value)}'`)
-          .join(' AND ')
-
-        whereClause = `WHERE ${conditions}`
-      }
-
-      const countSql = `SELECT COUNT(*) AS total FROM "${this.tableName}" ${whereClause}`
-      const countResult = (await db.prepare(countSql)).get() as { total: number }
-      page.pageNumber = countResult.total
-
-      const offset = (page.pageNumber - 1) * page.pageSize
-      const selectSql = `
-        SELECT *
-        FROM "${this.tableName}"
-        ${whereClause}
-        LIMIT ${page.pageSize} OFFSET ${offset}
-      `
-      const rows = (await db.prepare(selectSql)).all()
-
-      page.data = rows.map((row) => this.rowToObject(row as Record<string, unknown>))
-      return page
-    } finally {
-      db.release()
-    }
-  }
-
-  async getById(id: PrimaryKey): Promise<T> {
+  async getById(id: PrimaryKey): Promise<Query> {
     const db = this.acquire()
     try {
       const statement = `select * from ${this.tableName} where ${this.getPrimaryKeyColumnName()} = @${this.getPrimaryKeyColumnName()}`
@@ -106,13 +76,62 @@ export abstract class AbstractBaseDao<T extends BaseModel> implements BaseDao<T>
       Object.entries(originResult).forEach(([key, value]) => {
         result[StringUtil.snakeToCamelCase(key)] = value
       })
-      return result as T
+      return result as Query
     } finally {
       db.release()
     }
   }
 
-  protected getWhereClauses(queryConditions: Partial<T>, alias?: string): Record<string, string> {
+  async selectPage(page: PageModel<Query, Result>): Promise<PageModel<Query, Result>> {
+    const db = this.acquire()
+    try {
+      // 生成where字句
+      let whereClause = ''
+      if (page.query) {
+        const conditions = Object.keys(page.query).map(
+          (key) => `"${StringUtil.camelToSnakeCase(key)}" = @${key}`
+        )
+
+        if (conditions) {
+          if (conditions.length == 1) {
+            whereClause = `WHERE ${conditions.toString()}`
+          }
+          if (conditions.length > 1) {
+            whereClause = `WHERE ${conditions.join(' AND ')}`
+          }
+        }
+      }
+
+      // 拼接查询语句
+      let selectSql = `SELECT * FROM "${this.tableName}" ${whereClause}`
+      selectSql = await this.pager(selectSql, whereClause, page)
+
+      const rows = (await db.prepare(selectSql)).all(page.query) as object[]
+
+      // 结果集中的元素的属性名从snakeCase转换为camelCase，并赋值给page.data
+      page.data = rows.map(
+        (row) =>
+          Object.fromEntries(
+            Object.entries(row).map(([k, v]) => [StringUtil.snakeToCamelCase(k), v])
+          ) as Result
+      )
+
+      return page
+    } finally {
+      db.release()
+    }
+  }
+
+  /**
+   * 获取where字句
+   * @param queryConditions 查询条件
+   * @param alias 所查询数据表的别名
+   * @protected
+   */
+  protected getWhereClauses(
+    queryConditions: Partial<Query>,
+    alias?: string
+  ): Record<string, string> {
     const whereClauses: Record<string, string> = {}
     if (queryConditions) {
       Object.entries(queryConditions).forEach(([key, value]) => {
@@ -129,13 +148,50 @@ export abstract class AbstractBaseDao<T extends BaseModel> implements BaseDao<T>
     return whereClauses
   }
 
-  protected abstract getPrimaryKeyColumnName(): string
-
-  protected rowToObject(row: Record<string, unknown>): T {
-    // 这里假设 T 是一个简单的对象类型，可以直接将 row 转换为 T 类型
-    // 如果 T 类型更复杂（如包含嵌套对象、自定义类型等），可能需要更复杂的转换逻辑
-    return row as T
+  /**
+   * 分页器（为第一个参数statement末端拼接分页字句并返回，最后一个参数page的dataCount和pageCount赋值）
+   * @param statement 需要分页的语句
+   * @param whereClause 分页语句的where字句
+   * @param page 分页配置
+   * @protected
+   */
+  protected async pager(
+    statement: string,
+    whereClause: string,
+    page: PageModel<Query, Result>
+  ): Promise<string> {
+    statement += ' ' + (await this.getPagingClause(whereClause, page))
+    return statement
   }
+
+  /**
+   * 获取分页字句，并给参数page的dataCount和pageCount赋值）
+   * @param whereClause 需要分页的语句的where字句
+   * @param page 分页配置
+   * @protected
+   */
+  protected async getPagingClause(
+    whereClause: string,
+    page: PageModel<Query, Result>
+  ): Promise<string> {
+    const db = this.acquire()
+    try {
+      // 查询数据总量，计算页码数量
+      const countSql = `SELECT COUNT(*) FROM "${this.tableName}" ${whereClause}`
+      const countResult = (await db.prepare(countSql)).get(page.query) as { total: number }
+      page.dataCount = countResult.total
+      page.pageCount = Math.ceil(countResult.total / page.pageSize)
+
+      // 计算偏移量
+      const offset = (page.pageNumber - 1) * page.pageSize
+
+      return `LIMIT ${page.pageSize} OFFSET ${offset}`
+    } finally {
+      db.release()
+    }
+  }
+
+  protected abstract getPrimaryKeyColumnName(): string
 
   protected acquire() {
     return new DB(this.childClassName)
