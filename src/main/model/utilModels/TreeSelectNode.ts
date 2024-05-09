031b7dadceee0dd5130981e6f8b09a5d61e40e50
lvfeng <lvfeng.org@gmail.com>
2024-05-09 16:14:18 +0800

    fix(主进程-TreeSelectNode.ts): TreeSelectNode的value字段改为包含number

--- src/main/model/utilModels/TreeSelectNode.ts
+++ src/main/model/utilModels/TreeSelectNode.ts
@@ -4,7 +4,7 @@ import TreeNode from './TreeNode'
  * 树形选择框选项
  */
 export default class TreeSelectNode extends TreeNode<TreeSelectNode> {
-  value: string | null | undefined
+  value: string | number | null | undefined
   label: string | null | undefined
   secondaryLabel: string | null | undefined
   extraData: object | null | undefined
