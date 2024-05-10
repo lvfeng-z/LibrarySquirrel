c4e2beb7f9786645a749850e1c20ff7ac62bb939
lvfeng <lvfeng.org@gmail.com>
2024-05-10 22:36:53 +0800

    feat(主进程-StringUtil.ts): 新增removePrefixIfPresent方法

--- src/main/util/StringUtil.ts
+++ src/main/util/StringUtil.ts
@@ -1,4 +1,7 @@
 function isBlank(input: string | null | undefined): boolean {
+  if (input === undefined || input === null) {
+    return true
+  }
   return !input || /^\s*$/.test(input)
 }
 
@@ -13,9 +16,25 @@ function camelToSnakeCase(str: string): string {
 function snakeToCamelCase(str: string): string {
   return str.replace(/(_\w)/g, (m) => m[1].toUpperCase())
 }
+
+function removePrefixIfPresent(str: string, target: string): string {
+  // 使用 trim() 方法去除字符串开头的空白字符
+  const trimmedStr = str.trim()
+
+  // 检查处理后的字符串是否以前五个字符为"from "
+  if (trimmedStr.startsWith(target)) {
+    // 如果是，计算target的长度，并使用slice从target之后的字符开始截取
+    return trimmedStr.slice(target.length)
+  } else {
+    // 否则，返回原字符串
+    return str
+  }
+}
+
 export default {
   isBlank,
   isNotBlank,
   camelToSnakeCase,
-  snakeToCamelCase
+  snakeToCamelCase,
+  removePrefixIfPresent
 }
