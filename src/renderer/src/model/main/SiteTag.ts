08c9683d190f2e84017dce81a5fe6abebc51a098
lvfeng <lvfeng.org@gmail.com>
2024-05-10 22:38:13 +0800

    feat(渲染进程-SiteTag.ts): 新增SiteTag

---
+++ src/renderer/src/model/main/SiteTag.ts
@@ -0,0 +1,34 @@
+/**
+ * 本地标签
+ */
+
+export default interface LocalTag {
+  /**
+   * 主键
+   */
+  id: number | undefined | null
+  /**
+   * 标签来源站点id
+   */
+  siteId: number | undefined | null
+  /**
+   * 站点中标签的id
+   */
+  siteTagId: string | undefined | null
+  /**
+   * 站点中标签的名称
+   */
+  siteTagName: string | undefined | null
+  /**
+   * 上级标签id
+   */
+  baseSiteTagId: string | undefined | null
+  /**
+   * 描述
+   */
+  description: string | undefined | null
+  /**
+   * 站点标签对应的本地标签id
+   */
+  localTagId: number | undefined | null
+}
