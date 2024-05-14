21b52356f89b24fe1b5f252fd58f67b052652fcb
lvfeng <lvfeng.org@gmail.com>
2024-05-14 15:15:54 +0800

    refactor(渲染进程-scrollableText.ts): 自定义指令scrollableTextDirective重命名为scrollableText

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -7,7 +7,7 @@ import './styles/margin-box.css'
 import './styles/rounded-borders.css'
 import './styles/scroll-text.css'
 import './styles/z-axis-layers.css'
-import scrollableTextDirective from './directives/scrollableTextDirective'
+import scrollableText from './directives/scrollableText'
 import { elementIconRegister } from './plugins/elementIcon'
 
 const app = createApp(App)
@@ -15,5 +15,5 @@ app.use(Element)
 // 全局注册 el-icon
 elementIconRegister(app)
 // 注册溢出文字播放滚动动画的自定义指令
-app.directive('scrollable', scrollableTextDirective)
+app.directive('scrollable', scrollableText)
 app.mount('#app')
