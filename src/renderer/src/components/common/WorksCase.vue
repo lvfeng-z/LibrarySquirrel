b326011c27f04b882918a4ade05206ccf3b81397
lvfeng <lvfeng.org@gmail.com>
2024-05-21 17:39:50 +0800

    feat(渲染进程-WorksCase.vue): 新增WorksCase

---
+++ src/renderer/src/components/common/WorksCase.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Works from '../../model/main/Works'
+import DisplayCase from './DisplayCase.vue'
+
+// props
+const props = defineProps<{
+  worksList: Works[]
+}>()
+</script>
+
+<template>
+  <el-scrollbar class="works-display-case">
+    <template v-for="works in props.worksList" :key="works.id">
+      <display-case :image-url="works.filePath as string"></display-case>
+    </template>
+  </el-scrollbar>
+</template>
+
+<style scoped>
+.works-display-case {
+  display: flex;
+}
+</style>
