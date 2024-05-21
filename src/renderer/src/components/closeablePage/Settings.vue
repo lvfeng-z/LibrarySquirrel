b19208d6c557b9c8dc45db8b925546d662677697
lvfeng <lvfeng.org@gmail.com>
2024-05-21 16:16:09 +0800

    test(渲染进程-Settings.vue): 测试设置页面

--- src/renderer/src/components/closeablePage/Settings.vue
+++ src/renderer/src/components/closeablePage/Settings.vue
@@ -2,28 +2,55 @@
 import BaseCloseablePage from './BaseCloseablePage.vue'
 import { onBeforeMount, reactive, ref } from 'vue'
 import lodash from 'lodash'
+
 //onBeforeMount
 onBeforeMount(() => {
-  getSettings()
+  loadSettings()
 })
 
 // 变量
-const settings = ref()
+const settings = ref({ workdir: '' })
 const oldSettings = ref()
 const apis = reactive({
   settingsGetSettings: window.api.settingsGetSettings,
-  settingsSaveSettings: window.api.settingsSaveSettings
+  settingsSaveSettings: window.api.settingsSaveSettings,
+  settingsResetSettings: window.api.settingsResetSettings
 })
 
 // 方法
-function getSettings() {
-  apis.settingsGetSettings().then((response) => {
-    settings.value = response
-    oldSettings.value = lodash.cloneDeep(response)
-  })
+// 加载设置
+async function loadSettings() {
+  settings.value = await apis.settingsGetSettings()
+  oldSettings.value = lodash.cloneDeep(settings.value)
 }
+// 保存设置
 function saveSettings() {
-  apis.settingsSaveSettings()
+  const a = getChangedProperties(settings.value, oldSettings.value)
+  apis.settingsSaveSettings(a)
+}
+// 获取已更改的设置
+function getChangedProperties(newVal: object, oldVal: object, root?: string) {
+  const changedProperties: { path: string; value: unknown }[] = []
+  for (const key of Object.keys(oldVal)) {
+    root = root === undefined ? key : root + '.' + key
+    if (Object.prototype.hasOwnProperty.call(newVal, key)) {
+      if (typeof oldVal[key] === 'object') {
+        getChangedProperties(newVal[key], oldVal[key], root)
+      } else {
+        if (oldVal[key] !== newVal[key]) {
+          changedProperties.push({ path: root, value: newVal[key] })
+        }
+      }
+    } else {
+      changedProperties.push({ path: root, value: undefined })
+    }
+  }
+
+  return changedProperties
+}
+// 所有设置重置为默认
+function resetSettings() {
+  apis.settingsResetSettings()
 }
 </script>
 
@@ -35,9 +62,8 @@ function saveSettings() {
       </el-collapse-item>
     </el-collapse>
     <el-button @click="saveSettings">保存</el-button>
+    <el-button @click="resetSettings">重置</el-button>
   </base-closeable-page>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
