6aeb5f892abdd6a86b3a8a9a1acbf5997ae05b82
lvfeng <lvfeng.org@gmail.com>
2024-05-21 16:10:02 +0800

    fix(渲染进程-ImageBox.vue): 移除ImageBox组件

--- src/renderer/src/components/common/DisplayCase.vue
+++ src/renderer/src/components/common/DisplayCase.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-
-import ImageBox from './ImageBox.vue'
 import WorksInfo from './worksInfo.vue'
 import AuthorInfo from './authorInfo.vue'
 </script>
 
 <template>
   <div class="display-case">
-    <image-box class="display-case-image-box"></image-box>
+    <el-image class="display-case-image-box" src="workdir-resource://1.png"></el-image>
     <works-info class="display-case-works-info"></works-info>
     <author-info class="display-case-author-info"></author-info>
   </div>
