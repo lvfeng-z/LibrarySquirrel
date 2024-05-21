c203ee668aaca4df7fe9bee3b8f4b66a8fdbdc0c
lvfeng <lvfeng.org@gmail.com>
2024-05-21 16:08:06 +0800

    fix(主进程-SettingsUtil.ts): 加载模板配置文件的代码提取至defaultSettings

--- src/main/util/SettingsUtil.ts
+++ src/main/util/SettingsUtil.ts
@@ -2,18 +2,28 @@ import Store from 'electron-store'
 import settingsTemplate from '../resources/settings/settingsTemplate.json'
 import LogUtil from './LogUtil.ts'
 
+/**
+ * 根据配置状态初始化设置，并创建全局设置对象
+ */
 function initializeSettingsConfig() {
   const settings = new Store()
   global.settings = settings
   if (!settings.get('initialized', false)) {
-    try {
-      settings.set(settingsTemplate)
-      settings.set('initialized', true)
-    } catch (error) {
-      settings.set('initialized', false)
-      LogUtil.error('SettingsUtil', String(error))
-      throw error
-    }
+    defaultSettings()
+  }
+}
+
+/**
+ * 将设置全部重置到默认值
+ */
+function defaultSettings() {
+  try {
+    global.settings.set(settingsTemplate)
+    global.settings.set('initialized', true)
+  } catch (error) {
+    global.settings.set('initialized', false)
+    LogUtil.error('SettingsUtil', String(error))
+    throw error
   }
 }
 
@@ -26,5 +36,6 @@ function fixSettings() {
 
 export default {
   initializeSettingsConfig,
+  defaultSettings,
   fixSettings
 }
