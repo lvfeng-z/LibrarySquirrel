8d48bbd0c572f423ab7a0336d4c861745fc3a23e
lvfeng <lvfeng.org@gmail.com>
2024-05-22 14:17:03 +0800

    feat(渲染进程-): DisplayCase现在可以控制宽高

--- src/renderer/src/components/common/DisplayCase.vue
+++ src/renderer/src/components/common/DisplayCase.vue
@@ -1,15 +1,39 @@
 <script setup lang="ts">
 import WorksInfo from './WorksInfo.vue'
 import AuthorInfo from './AuthorInfo.vue'
+import { onBeforeMount, Ref, ref, UnwrapRef } from 'vue'
 
 // props
 const props = defineProps<{
   imageUrl: string
+  height?: number
+  width?: number
 }>()
+
+// onBeforeMount
+onBeforeMount(() => {
+  handleElImageFit()
+})
+
+// 变量
+const imageFit: Ref<UnwrapRef<'contain' | 'cover' | 'fill' | 'none' | 'scale-down'>> = ref('none')
+const imageWidth: Ref<UnwrapRef<string>> = ref(
+  props.width === undefined ? 'auto' : String(props.width) + 'px'
+)
+const imageHeight: Ref<UnwrapRef<string>> = ref(
+  props.width === undefined ? 'auto' : String(props.height) + 'px'
+)
+
+// 方法
+// 判断el-image使用什么模式
+function handleElImageFit() {
+  imageFit.value = 'none'
+}
 </script>
 <template>
   <div class="display-case">
     <el-image
+      :fit="imageFit"
       class="display-case-image-box"
       :src="`workdir-resource://workdir/${props.imageUrl}`"
     ></el-image>
@@ -22,6 +46,8 @@ const props = defineProps<{
 .display-case {
   display: flex;
   flex-direction: column;
+  width: v-bind(imageWidth);
+  height: v-bind(imageHeight);
 }
 .display-case-image-box {
   width: 100%;
