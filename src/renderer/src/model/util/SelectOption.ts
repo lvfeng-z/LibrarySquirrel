4ca53e9c5eff34ac458b6fe573e7eb95b6c19f1b
lvfeng <lvfeng.org@gmail.com>
2024-05-23 14:58:55 +0800

    feat(渲染进程-): SelectOption新增state属性，用于表示double-check-tag的状态

--- src/renderer/src/model/util/SelectOption.ts
+++ src/renderer/src/model/util/SelectOption.ts
@@ -1,7 +1,8 @@
 interface SelectOption {
   value: string
   label: string
-  secondaryLabel: string
+  secondaryLabel: string // 用于double-check-tag
+  state?: boolean // 用于double-check-tag
   extraData?: object
 }
 
