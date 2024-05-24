e65ce62fa53cec8777bea8d5880898330c750973
lvfeng <lvfeng.org@gmail.com>
2024-05-25 05:04:15 +0800

    feat(主进程-TaskHandler.ts): 新增create接口

--- src/main/plugin/TaskHandler.ts
+++ src/main/plugin/TaskHandler.ts
@@ -2,6 +2,12 @@ import Task from '../model/Task.ts'
 
 export default interface TaskHandler {
   /**
+   * 创建任务
+   * @return 下载的资源
+   */
+  create(task: Task): Promise<Task[]>
+
+  /**
    * 开始任务
    * @return 下载的资源
    */
