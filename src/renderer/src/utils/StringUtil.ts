1cd7cdc830b70461b889523911f2a9159612de56
lvfeng <lvfeng.org@gmail.com>
2024-05-24 23:10:08 +0800

    feat(主进程、渲染进程-StringUtil.ts): 新增createObjectsFromRegexMatches方法

--- src/renderer/src/utils/StringUtil.ts
+++ src/renderer/src/utils/StringUtil.ts
@@ -31,10 +31,45 @@ function removePrefixIfPresent(str: string, target: string): string {
   }
 }
 
+/**
+ * 给定一个正则表达式，它包含一个或多个捕获组；一段文本字符串，需要从中查找与正则表达式匹配的部分；以及一个属性名数组，用于标记捕获组数据的属性。执行以下步骤：
+ * 1. 使用正则表达式在文本字符串中查找所有匹配项。
+ * 2. 对于每个匹配项，将其捕获组的内容提取出来。
+ * 3. 将每个捕获组的内容与属性数组中的相应元素配对。第一个捕获组的内容对应属性数组的第一个元素，第二个捕获组对应第二个元素，依此类推。
+ * 4. 为每个匹配创建一个对象，其中键是从属性数组中取的，值则是对应的捕获组内容。
+ * 5. 将所有这些对象收集到一个数组中，并返回这个数组作为最终结果。
+ */
+function createObjectsFromRegexMatches(
+  regexPattern: string,
+  targetString: string,
+  propertiesArray: string[]
+): object[] {
+  const regex = new RegExp(regexPattern, 'g')
+  let match
+  const resultObjects: object[] = []
+
+  while ((match = regex.exec(targetString)) !== null) {
+    if (match.length - 1 === propertiesArray.length) {
+      // 确保捕获组数量与属性数组长度一致
+      const obj = {}
+      propertiesArray.forEach((propertyName, index) => {
+        obj[propertyName] = match[index + 1] // 匹配值从index 1开始，因为index 0是整个匹配的字符串
+      })
+      resultObjects.push(obj)
+    } else {
+      const msg = `捕获组数量与属性数组长度不匹配，请检查正则表达式与属性名数组是否匹配。regexPattern: ${regexPattern} ; propertiesArray: ${propertiesArray}`
+      throw new Error(msg)
+    }
+  }
+
+  return resultObjects
+}
+
 export default {
   isBlank,
   isNotBlank,
   camelToSnakeCase,
   snakeToCamelCase,
-  removePrefixIfPresent
+  removePrefixIfPresent,
+  createObjectsFromRegexMatches
 }
