92535f7f4a6d9ba642823022e287304ab2a2e0bc
lvfeng <lvfeng.org@gmail.com>
2024-05-25 22:51:39 +0800

    test(主进程-TaskService.ts): 测试使用插件方式的任务开始功能

--- resources/plugins/task/lvfeng/www.pixiv.net/1.0.0/code/pixivTaskHandler.mjs
+++ resources/plugins/task/lvfeng/www.pixiv.net/1.0.0/code/pixivTaskHandler.mjs
@@ -1,12 +1,56 @@
-import fs from 'fs/promises'
+import fs from 'fs'
 
 export default class PixivTaskHandler {
   constructor() {
   }
 
-  start(task) {
-    console.log(task)
-     // 异步读取文件
-    return fs.readFile('C:/asf.png') // 返回文件
+  create(url) {
+
+  }
+
+  async start(tasks) {
+    const worksDTOs = []
+    tasks.forEach((task) => {
+      const worksDTO = this.createWorksDTO()
+      worksDTO.siteWorksName = ''
+      worksDTO.siteWorkDescription = ''
+      worksDTO.includeTaskId = task.id
+      // 异步读取文件
+      worksDTO.resourceStream = fs.createReadStream(task.url)
+      worksDTOs.push(worksDTO)
+    })
+    return worksDTOs
+  }
+
+  pause() {
+
+  }
+
+  cancel() {
+
+  }
+
+  createWorksDTO() {
+    return {
+      filePath: undefined,
+      workdir: undefined,
+      worksType: undefined,
+      siteId: undefined,
+      siteWorksId: undefined,
+      siteWorksName: undefined,
+      siteAuthorId: undefined,
+      siteWorkDescription: undefined,
+      siteUploadTime: undefined,
+      siteUpdateTime: undefined,
+      nickName: undefined,
+      localAuthorId: undefined,
+      includeTime: undefined,
+      includeMode: undefined,
+      includeTaskId: undefined,
+      downloadStatus: undefined,
+      author: undefined,
+      siteTags: undefined,
+      resourceStream: undefined
+    }
   }
 }
--- src/main/service/TaskService.ts
+++ src/main/service/TaskService.ts
@@ -1,36 +1,96 @@
 import Task from '../model/Task.ts'
 import LogUtil from '../util/LogUtil.ts'
-import InstalledPluginsService from './InstalledPluginsService.ts'
 import TaskHandler from '../plugin/TaskHandler.ts'
 import TaskDao from '../dao/TaskDao.ts'
-import fs from 'fs/promises'
+import fs from 'fs'
 import SettingsService from './SettingsService.ts'
+import PluginLoader from '../plugin/PluginLoader.ts'
+import TaskConstant from '../constant/TaskConstant.ts'
+import TaskQueryDTO from '../model/queryDTO/TaskQueryDTO.ts'
+import InstalledPluginsService from './InstalledPluginsService.ts'
 
-async function createTask(task: Task): Promise<number> {
+/**
+ * 保存任务
+ * @param task
+ */
+async function save(task: Task): Promise<number> {
   task = new Task(task)
   const dao = new TaskDao()
   return (await dao.save(task)) as number
 }
 
+/**
+ * 根据传入的url创建任务
+ * @param url 作品/作品集所在url
+ */
+async function createTask(url: string) {
+  console.log(url)
+  // task = new Task(task)
+  // const dao = new TaskDao()
+  // return (await dao.save(task)) as number
+}
+
+/**
+ * 开始任务
+ * @param taskId
+ */
 async function startTask(taskId: number): Promise<boolean> {
   const dao = new TaskDao()
   const task = await dao.getById(taskId)
 
+  if (task === undefined) {
+    const msg = `找不到任务，taskId = ${taskId}`
+    LogUtil.error('TaskService', msg)
+    return false
+  }
   if (!Object.prototype.hasOwnProperty.call(task, 'siteId') || task.siteId === 0) {
     const msg = '开始任务时，siteId意外为空'
     LogUtil.error('TaskService', msg, 'task:', task)
     return false
   }
 
+  let tasks: Task[] = []
+
+  if (task.isCollection) {
+    tasks = await getChildrenTask(task.id as number)
+  }
+
   const pluginId = global.settings.get(`plugins.task.${task.siteId}`)
-  const classPath = await InstalledPluginsService.getClassPathById(pluginId)
-  return import(classPath).then(async (module) => {
+
+  // 查询插件信息，日志用
+  const pluginInfo = JSON.stringify(await InstalledPluginsService.getById(pluginId))
+
+  const pluginLoader = new PluginLoader()
+  return pluginLoader.loadTaskPlugin(pluginId).then(async (taskHandler: TaskHandler) => {
     try {
-      const taskHandler = new module.default() as TaskHandler
-      const data = await taskHandler.start(task)
+      const worksDTOs = await taskHandler.start(tasks)
 
       const settings = SettingsService.getSettings() as { workdir: string }
-      fs.writeFile(`${settings.workdir}/test.jpg`, data)
+
+      let i = 0
+      worksDTOs.forEach((worksDTO) => {
+        if (
+          Object.prototype.hasOwnProperty.call(worksDTO, 'resourceStream') &&
+          worksDTO.resourceStream !== undefined &&
+          worksDTO.resourceStream !== null
+        ) {
+          const writeStream = fs.createWriteStream(`${settings.workdir}/download/test${i}.jpg`)
+          worksDTO.resourceStream.pipe(writeStream)
+
+          worksDTO.resourceStream.on('end', () => {
+            finishTask(worksDTO.includeTaskId as number)
+          })
+          worksDTO.resourceStream.on('error', () => {
+            taskFailed(worksDTO.includeTaskId as number)
+          })
+          i++
+        } else {
+          LogUtil.warn(
+            'TaskService',
+            `插件未返回任务的资源，taskId: ${worksDTO.includeTaskId}，plugin: ${pluginInfo}`
+          )
+        }
+      })
       return true
     } catch (err) {
       LogUtil.error('TaskService', err)
@@ -39,7 +99,37 @@ async function startTask(taskId: number): Promise<boolean> {
   })
 }
 
+function getChildrenTask(parentId: number) {
+  const dao = new TaskDao()
+  const query = new TaskQueryDTO()
+  query.parentId = parentId
+  return dao.selectList(query)
+}
+
+/**
+ * 任务完成
+ * @param taskId
+ */
+function finishTask(taskId: number) {
+  const dao = new TaskDao()
+  const task = new Task()
+  task.status = TaskConstant.TaskStatesEnum.finished
+  return dao.updateById(taskId, task)
+}
+
+/**
+ * 任务失败
+ * @param taskId
+ */
+function taskFailed(taskId: number) {
+  const dao = new TaskDao()
+  const task = new Task()
+  task.status = TaskConstant.TaskStatesEnum.failed
+  return dao.updateById(taskId, task)
+}
+
 export default {
+  save,
   createTask,
   startTask
 }
