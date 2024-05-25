07d05e51d496c832b06eb9ca51a076350bb45e9f
lvfeng <lvfeng.org@gmail.com>
2024-05-25 22:49:41 +0800

    fix(主进程-PluginLoader.ts、TaskHandler.ts): TaskHandler补充pause和cancel方法

--- src/main/plugin/PluginLoader.ts
+++ src/main/plugin/PluginLoader.ts
@@ -10,10 +10,12 @@ export default class PluginLoader {
 
     // 验证taskPlugin是否符合TaskHandler接口要求
     let isTaskHandler: boolean
+    // 查询插件信息，日志用
+    const pluginInfo = JSON.stringify(await InstalledPluginsService.getById(pluginId))
     // create方法
     isTaskHandler = 'create' in taskPlugin && typeof taskPlugin.create === 'function'
     if (!isTaskHandler) {
-      const msg = `加载任务插件时出错，插件${classPath}未实现create方法`
+      const msg = `加载任务插件时出错，插件${pluginInfo}未实现create方法`
       LogUtil.error('PluginLoader', msg)
       throw new Error(msg)
     }
@@ -21,7 +23,23 @@ export default class PluginLoader {
     // start方法
     isTaskHandler = 'start' in taskPlugin && typeof taskPlugin.start === 'function'
     if (!isTaskHandler) {
-      const msg = `加载任务插件时出错，插件${classPath}未实现start方法`
+      const msg = `加载任务插件时出错，插件${pluginInfo}未实现start方法`
+      LogUtil.error('PluginLoader', msg)
+      throw new Error(msg)
+    }
+
+    // pause方法
+    isTaskHandler = 'pause' in taskPlugin && typeof taskPlugin.pause === 'function'
+    if (!isTaskHandler) {
+      const msg = `加载任务插件时出错，插件${pluginInfo}未实现pause方法`
+      LogUtil.error('PluginLoader', msg)
+      throw new Error(msg)
+    }
+
+    // cancel方法
+    isTaskHandler = 'cancel' in taskPlugin && typeof taskPlugin.cancel === 'function'
+    if (!isTaskHandler) {
+      const msg = `加载任务插件时出错，插件${pluginInfo}未实现cancel方法`
       LogUtil.error('PluginLoader', msg)
       throw new Error(msg)
     }
--- src/main/plugin/TaskHandler.ts
+++ src/main/plugin/TaskHandler.ts
@@ -1,15 +1,28 @@
 import Task from '../model/Task.ts'
+import WorksDTO from '../model/dto/WorksDTO.ts'
 
 export default interface TaskHandler {
   /**
    * 创建任务
    * @return 下载的资源
    */
-  create(task: Task): Promise<Task[]>
+  create(url: string): Promise<Task[]>
 
   /**
    * 开始任务
    * @return 下载的资源
    */
-  start(task: Task): Promise<Buffer>
+  start(tasks: Task[]): Promise<WorksDTO[]>
+
+  /**
+   * 暂停下载任务
+   * @param taskIds
+   */
+  pause(taskIds: number[]): Promise<boolean>
+
+  /**
+   * 取消下载任务
+   * @param taskIds
+   */
+  cancel(taskIds: number[]): Promise<boolean>
 }
