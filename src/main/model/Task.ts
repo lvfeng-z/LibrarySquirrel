13b12c6eb813d20efb825e3b8ff40b9de96307ed
lvfeng <lvfeng.org@gmail.com>
2024-05-25 18:36:27 +0800

    fix(主进程-createDataTables.yml、Task.ts、TaskHandler.ts): Task表新增has_children、parent_id、site_works_id、plugin_data字段，works_id改为local_works_id，实体类等数据模型同步调整

--- src/main/model/Task.ts
+++ src/main/model/Task.ts
@@ -5,13 +5,25 @@ import BaseModel from './BaseModel.ts'
  */
 export default class Task extends BaseModel {
   /**
-   * 任务链接的站点id
+   * 是否有子任务
+   */
+  hasChildren: boolean | undefined | null
+  /**
+   * 上级任务id
+   */
+  parentId: number | undefined | null
+  /**
+   * 任务的站点id
    */
   siteId: number | undefined | null
   /**
-   * 作品id
+   * 本地作品id
    */
-  worksId: number | undefined | null
+  localWorksId: number | undefined | null
+  /**
+   * 站点作品id
+   */
+  siteWorksId: number | undefined | null
   /**
    * 链接
    */
@@ -20,20 +32,36 @@ export default class Task extends BaseModel {
    * 状态（0：未开始，1：进行中，2：暂停，3：已完成，4：失败）
    */
   status: number | undefined | null
+  /**
+   * 插件数据
+   */
+  pluginData: { [key: string]: unknown } | undefined | null
 
   constructor(task?: Task) {
     if (task === undefined) {
       super()
+      this.hasChildren = undefined
+      this.parentId = undefined
       this.siteId = undefined
-      this.worksId = undefined
+      this.localWorksId = undefined
+      this.siteWorksId = undefined
       this.url = undefined
       this.status = undefined
+      this.pluginData = undefined
     } else {
       super(task)
+      this.hasChildren = task.hasChildren
+      this.parentId = task.parentId
       this.siteId = task.siteId
-      this.worksId = task.worksId
+      this.localWorksId = task.localWorksId
+      this.siteWorksId = task.siteWorksId
       this.url = task.url
       this.status = task.status
+      if (typeof task.pluginData === 'string') {
+        this.pluginData = JSON.parse(task.pluginData)
+      } else {
+        this.pluginData = task.pluginData
+      }
     }
   }
 }
