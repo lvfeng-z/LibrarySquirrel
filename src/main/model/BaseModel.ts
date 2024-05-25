9501d1003604fd00499e5bef54a5cbd6dbb7450b
lvfeng <lvfeng.org@gmail.com>
2024-05-25 22:43:24 +0800

    fix(主进程-BaseModel.ts): 取消无参构造将创建时间和更新时间设为当前时间

--- src/main/model/BaseModel.ts
+++ src/main/model/BaseModel.ts
@@ -20,8 +20,8 @@ export default class BaseModel {
   constructor(baseModel?: BaseModel) {
     if (baseModel === undefined) {
       this.id = undefined
-      this.createTime = Date.now()
-      this.updateTime = Date.now()
+      this.createTime = undefined
+      this.updateTime = undefined
     } else {
       this.id = baseModel.id
       this.createTime = baseModel.createTime
