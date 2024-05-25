559d2091cd1c7db5272ecfa50745e1dbd0243852
lvfeng <lvfeng.org@gmail.com>
2024-05-25 18:40:30 +0800

    fix(主进程-WorksDTO.ts): WorksDTO新增siteTags和resourceBuffer属性

--- src/main/model/dto/WorksDTO.ts
+++ src/main/model/dto/WorksDTO.ts
@@ -1,5 +1,6 @@
 import Works from '../Works.ts'
 import LocalAuthor from '../LocalAuthor.ts'
+import SiteTag from '../SiteTag.ts'
 
 /**
  * 作品
@@ -10,10 +11,22 @@ export default class WorksDTO extends Works {
    */
   author: LocalAuthor | undefined | null
 
+  /**
+   * 站点标签数组
+   */
+  siteTags: SiteTag[] | undefined | null
+
+  /**
+   * 作品资源的数据流
+   */
+  resourceBuffer: Buffer | undefined | null
+
   constructor(works?: WorksDTO) {
     if (works === undefined) {
       super()
       this.author = undefined
+      this.siteTags = undefined
+      this.resourceBuffer = undefined
     } else {
       super(works)
       if (typeof works.author === 'string') {
@@ -21,6 +34,8 @@ export default class WorksDTO extends Works {
       } else {
         this.author = works.author
       }
+      this.siteTags = works.siteTags
+      this.resourceBuffer = works.resourceBuffer
     }
   }
 }
