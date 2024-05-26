bef88971efe2cd8869a1f999c2f132889dfe7db2
lvfeng <lvfeng.org@gmail.com>
2024-05-26 20:43:44 +0800

    feat(主进程-ObjectUtil.ts): 新增undefinedToNull、alignProperties方法

--- src/main/util/ObjectUtil.ts
+++ src/main/util/ObjectUtil.ts
@@ -1,3 +1,7 @@
+/**
+ * 去除值为undefined的属性
+ * @param obj
+ */
 function nonUndefinedValue(obj: object | undefined): object {
   if (obj === undefined) {
     return {}
@@ -9,6 +13,45 @@ function nonUndefinedValue(obj: object | undefined): object {
   )
 }
 
+/**
+ * 把所有undefined的值改为null
+ * @param obj
+ */
+function undefinedToNull(obj: object | undefined): object {
+  if (obj === undefined) {
+    return {}
+  }
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => {
+      if (value === undefined) {
+        return [key, null]
+      } else {
+        return [key, value]
+      }
+    })
+  )
+}
+
+/**
+ * 对其对象列表的属性
+ * @param objects 对象列表
+ * @param fill 如何填充被对齐的值
+ */
+function alignProperties(objects: object[], fill: unknown) {
+  // 所有可能的属性名集合（这里需要你根据实际情况定义）
+  const allPossibleProperties = [...new Set(objects.flatMap(Object.keys))]
+  // 创建一个模板对象，所有属性设为fill
+  const templateObject = allPossibleProperties.reduce((acc, key) => ({ ...acc, [key]: fill }), {})
+
+  // 遍历对象列表，补全每个对象的属性
+  return objects.map((obj) => {
+    // 合并当前对象与模板对象，模板对象中的属性会覆盖或补充到当前对象中
+    return { ...templateObject, ...obj }
+  })
+}
+
 export default {
-  nonUndefinedValue
+  nonUndefinedValue,
+  undefinedToNull,
+  alignProperties
 }
