30edb125db43345bd6298215f97128cc4d63bf90
lvfeng <lvfeng.org@gmail.com>
2024-05-26 21:33:59 +0800

    fix(主进程-createDataTables.yml、Task.ts、TaskQueryDTO.ts): Task表新增plugin_id、plugin_info字段，用于记录是哪个插件创建了任务

--- src/main/model/Task.ts
+++ src/main/model/Task.ts
@@ -8,30 +8,47 @@ export default class Task extends BaseModel {
    * 是否是任务集合
    */
   isCollection: boolean | undefined | null
+
   /**
    * 上级任务id
    */
   parentId: number | undefined | null
+
   /**
    * 任务的站点id
    */
   siteId: number | undefined | null
+
   /**
    * 本地作品id/本地作品集id
    */
   localWorksId: number | undefined | null
+
   /**
    * 站点作品id
    */
   siteWorksId: number | undefined | null
+
   /**
    * 当任务是任务集合时，url存储创建此任务集合所使用的url，否则存储的是下载资源的链接（由于存在临时的下载链接，此字段可能没有作用）
    */
   url: string | undefined | null
+
   /**
    * 状态（0：未开始，1：进行中，2：暂停，3：已完成，4：失败）
    */
   status: number | undefined | null
+
+  /**
+   * 插件id
+   */
+  pluginId: number | undefined | null
+
+  /**
+   * 插件信息
+   */
+  pluginInfo: number | undefined | null
+
   /**
    * 插件数据
    */
@@ -47,6 +64,8 @@ export default class Task extends BaseModel {
       this.siteWorksId = undefined
       this.url = undefined
       this.status = undefined
+      this.pluginId = undefined
+      this.pluginInfo = undefined
       this.pluginData = undefined
     } else {
       super(task)
@@ -57,6 +76,8 @@ export default class Task extends BaseModel {
       this.siteWorksId = task.siteWorksId
       this.url = task.url
       this.status = task.status
+      this.pluginId = task.pluginId
+      this.pluginInfo = task.pluginInfo
       if (typeof task.pluginData === 'string') {
         this.pluginData = JSON.parse(task.pluginData)
       } else {
@@ -77,5 +98,7 @@ export default class Task extends BaseModel {
     this.siteId = undefined
     this.localWorksId = undefined
     this.status = undefined
+    this.pluginId = undefined
+    this.pluginInfo = undefined
   }
 }
