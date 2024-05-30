68330c79dcf3d85c19287174d83b69ec20929425
lvfeng <lvfeng.org@gmail.com>
2024-05-30 14:13:06 +0800

    feat(渲染进程-TaskManage.vue): 新增TaskManage页面

---
+++ src/renderer/src/components/closeablePage/TaskManage.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import BaseCloseablePage from './BaseCloseablePage.vue'
+import { reactive, ref } from 'vue'
+
+// 变量
+const apis = reactive({
+  taskServiceCreateTask: window.api.taskServiceCreateTask
+}) // 接口
+const activeName = ref([1]) // 默认展开的折叠面板
+const importDir = ref('')
+
+// 方法
+// 保存设置
+function importFromDir() {
+  apis.taskServiceCreateTask('file://'.concat(importDir.value))
+}
+</script>
+
+<template>
+  <base-closeable-page>
+    <el-scrollbar>
+      <el-collapse v-model="activeName">
+        <el-collapse-item title="本地导入" :name="1">
+          <el-input v-model="importDir"></el-input>
+        </el-collapse-item>
+      </el-collapse>
+      <el-row>
+        <el-col :span="6">
+          <el-button type="primary" @click="importFromDir">导入</el-button>
+        </el-col>
+      </el-row>
+    </el-scrollbar>
+  </base-closeable-page>
+</template>
+
+<style scoped></style>
