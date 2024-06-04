ab0151bcde61396aeb8f91e76a7887c28d65edfe
lvfeng <lvfeng.org@gmail.com>
2024-06-04 16:41:58 +0800

    test: 测试插件开始任务

--- src/renderer/src/components/closeablePage/TaskManage.vue
+++ src/renderer/src/components/closeablePage/TaskManage.vue
@@ -4,10 +4,12 @@ import { reactive, ref } from 'vue'
 
 // 变量
 const apis = reactive({
-  taskServiceCreateTask: window.api.taskServiceCreateTask
+  taskServiceCreateTask: window.api.taskServiceCreateTask,
+  taskServiceStartTask: window.api.taskServiceStartTask
 }) // 接口
-const activeName = ref([1]) // 默认展开的折叠面板
-const importDir = ref('')
+const activeName = ref([1, 2]) // 默认展开的折叠面板
+const importDir = ref('') // 导入目录
+const taskId = ref() // 任务id
 
 // 方法
 // 保存设置
@@ -16,6 +18,10 @@ function importFromDir() {
     console.log('完成了')
   })
 }
+// 开始任务
+function startTask() {
+  apis.taskServiceStartTask(taskId.value)
+}
 </script>
 
 <template>
@@ -23,14 +29,30 @@ function importFromDir() {
     <el-scrollbar>
       <el-collapse v-model="activeName">
         <el-collapse-item title="本地导入" :name="1">
-          <el-input v-model="importDir"></el-input>
+          <el-row>
+            <el-col :span="24">
+              <el-input v-model="importDir"></el-input>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="3">
+              <el-button type="primary" @click="importFromDir">导入</el-button>
+            </el-col>
+          </el-row>
+        </el-collapse-item>
+        <el-collapse-item title="开始任务" :name="2">
+          <el-row>
+            <el-col :span="24">
+              <el-input v-model="taskId"></el-input>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="3">
+              <el-button type="primary" @click="startTask">开始任务</el-button>
+            </el-col>
+          </el-row>
         </el-collapse-item>
       </el-collapse>
-      <el-row>
-        <el-col :span="6">
-          <el-button type="primary" @click="importFromDir">导入</el-button>
-        </el-col>
-      </el-row>
     </el-scrollbar>
   </base-closeable-page>
 </template>
