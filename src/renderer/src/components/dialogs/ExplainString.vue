fc41463ebc8563fc97e2c3edfd69578862292cf3
lvfeng <lvfeng.org@gmail.com>
2024-06-05 17:32:31 +0800

    feat(渲染进程-ExplainString.vue): 新增ExplainString，用于导入作品时解释目录意义

---
+++ src/renderer/src/components/dialogs/ExplainString.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// props
+const props = defineProps<{
+  stringToExplain: string
+}>()
+</script>
+
+<template>
+  <el-dialog>
+    <el-row>
+      <label>{{ props.stringToExplain }}</label>
+    </el-row>
+    <el-row>
+      <el-select></el-select>
+    </el-row>
+  </el-dialog>
+</template>
+
+<style scoped></style>
