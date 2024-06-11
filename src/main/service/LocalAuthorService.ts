508bc4d370a6fcf295537b7d482d18ebbd7ecff5
lvfeng <lvfeng.org@gmail.com>
2024-06-11 17:28:41 +0800

    feat(主进程-LocalAuthorService.ts): 新增getSelectItems方法

--- src/main/service/LocalAuthorService.ts
+++ src/main/service/LocalAuthorService.ts
@@ -3,6 +3,8 @@ import LocalAuthorQueryDTO from '../model/queryDTO/LocalAuthorQueryDTO.ts'
 import LocalAuthor from '../model/LocalAuthor.ts'
 import BaseDao from '../dao/BaseDao.ts'
 import LocalAuthorDao from '../dao/LocalAuthorDao.ts'
+import SelectItem from '../model/utilModels/SelectItem.ts'
+import LogUtil from '../util/LogUtil.ts'
 
 /**
  * 本地作者Service
@@ -10,6 +12,18 @@ import LocalAuthorDao from '../dao/LocalAuthorDao.ts'
 export default class LocalAuthorService extends BaseService<LocalAuthorQueryDTO, LocalAuthor> {
   constructor() {
     super('LocalAuthorService')
+  }
+
+  /**
+   * 查询SelectItem列表
+   */
+  public getSelectItems(): Promise<SelectItem> {
+    const dao = new LocalAuthorDao()
+    try {
+      return dao.getSelectItems()
+    } catch (error) {
+      LogUtil.error('LocalAuthorService', error)
+    }
   }
 
   protected getDao(): BaseDao<LocalAuthorQueryDTO, LocalAuthor> {
