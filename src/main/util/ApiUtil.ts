c46ed618145abee4dc37a7039854be7c35ce2005
lvfeng <lvfeng.org@gmail.com>
2024-06-12 14:51:05 +0800

    docs: 补充文档

--- src/main/util/ApiUtil.ts
+++ src/main/util/ApiUtil.ts
@@ -1,7 +1,19 @@
 export default class ApiUtil {
+  /**
+   * 是否成功
+   */
   success: boolean | undefined
+
+  /**
+   * 响应信息
+   */
   msg: string | undefined
+
+  /**
+   * 数据
+   */
   data: unknown | undefined
+
   constructor() {
     this.success = undefined
     this.msg = undefined
