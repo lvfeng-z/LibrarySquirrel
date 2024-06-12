2da16298fe23720076552fc345ea22d6324f3f9d
lvfeng <lvfeng.org@gmail.com>
2024-06-12 23:23:50 +0800

    fix(主进程-PluginLoader.ts): 构造函数中插件工具的事件处理提取到私有方法中

--- src/main/plugin/PluginLoader.ts
+++ src/main/plugin/PluginLoader.ts
@@ -4,6 +4,9 @@ import LogUtil from '../util/LogUtil.ts'
 import PluginTool from './PluginTool.ts'
 import Electron from 'electron'
 import { EventEmitter } from 'node:events'
+import { MeaningOfPath } from '../model/utilModels/MeaningOfPath.ts'
+import LocalAuthorService from '../service/LocalAuthorService.ts'
+import LocalTagService from '../service/LocalTagService.ts'
 
 export default class PluginLoader {
   /**
@@ -17,22 +20,16 @@ export default class PluginLoader {
 
   constructor(mainWindow: Electron.BrowserWindow) {
     this.mainWindow = mainWindow
+    const event = new EventEmitter()
 
-    const events = new EventEmitter()
-
-    // 处理插件工具的explain-path-request事件
-    events.on('explain-path-request', (dir) => {
-      // 监听渲染进程的explain-path-response事件
-      Electron.ipcMain.once('explain-path-response', (_event, args) => {
-        events.emit('explain-path-response', args)
-      })
-      // 向渲染进程发送explain-path-request事件
-      this.mainWindow.webContents.send('explain-path-request', dir)
-    })
-
-    this.pluginTool = new PluginTool(events)
+    this.attachExplainPathEvents(event)
+    this.pluginTool = new PluginTool(event)
   }
 
+  /**
+   * 加载任务插件
+   * @param pluginId
+   */
   public async loadTaskPlugin(pluginId: number) {
     const classPath = await InstalledPluginsService.getClassPathById(pluginId)
     const module = await import(classPath)
@@ -67,5 +64,45 @@ export default class PluginLoader {
     }
 
     return taskPlugin as TaskHandler
+  }
+
+  /**
+   * 为事件发射器附加用户解释目录含义的一系列事件和处理函数
+   * @param event
+   * @private
+   */
+  private async attachExplainPathEvents(event: EventEmitter) {
+    // 处理插件工具的explain-path-request事件
+    event.on('explain-path-request', (dir) => {
+      // 监听渲染进程的explain-path-response事件
+      Electron.ipcMain.once(
+        'explain-path-response',
+        async (_event, meaningOfPaths: MeaningOfPath[]) => {
+          for (const meaningOfPath of meaningOfPaths) {
+            if (meaningOfPath.id === undefined || meaningOfPath.id === null) {
+              const msg = '获取目录含义的详情时，id意外为空，'
+              LogUtil.error('PluginLoader', msg)
+              throw new Error(msg)
+            }
+
+            if (meaningOfPath.type === 'author') {
+              const localAuthorService = new LocalAuthorService()
+              const localAuthor = await localAuthorService.getById(meaningOfPath.id)
+              meaningOfPath.name = localAuthor.localAuthorName
+              meaningOfPath.details = localAuthor
+            }
+            if (meaningOfPath.type === 'tag') {
+              const localTagService = new LocalTagService()
+              const localTag = await localTagService.getById(meaningOfPath.id)
+              meaningOfPath.name = localTag.localTagName
+              meaningOfPath.details = localTag
+            }
+          }
+          event.emit('explain-path-response', meaningOfPaths)
+        }
+      )
+      // 向渲染进程发送explain-path-request事件
+      this.mainWindow.webContents.send('explain-path-request', dir)
+    })
   }
 }
