0d7fc03afc41035abe9c0f0d409e65d41c80f9ff
lvfeng <lvfeng.org@gmail.com>
2024-06-15 20:04:43 +0800

    refactor(渲染进程-LocalTag.ts): LocalTag重构为类

--- src/renderer/src/model/main/LocalTag.ts
+++ src/renderer/src/model/main/LocalTag.ts
@@ -1,29 +1,32 @@
+import BaseModel from './BaseModel.ts'
+
 /**
  * 本地标签
  */
-
-export default interface LocalTag {
+export default class LocalTag extends BaseModel {
   /**
    * 主键
    */
   id: number | null | undefined
   /**
-   * 创建时间
-   */
-  createTime: string | null | undefined
-
-  /**
-   * 更新时间
-   */
-  updateTime: string | null | undefined
-
-  /**
    * 本地标签名称
    */
   localTagName: string | null | undefined
-
   /**
    * 上级标签id
    */
   baseLocalTagId: number | null | undefined
+  constructor(localTag?: LocalTag) {
+    if (localTag === undefined) {
+      super()
+      this.id = undefined
+      this.localTagName = undefined
+      this.baseLocalTagId = undefined
+    } else {
+      super(localTag)
+      this.id = localTag.id
+      this.localTagName = localTag.localTagName
+      this.baseLocalTagId = localTag.baseLocalTagId
+    }
+  }
 }
