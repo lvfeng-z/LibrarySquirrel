d99623800db40980d29450fca1e041a2a985d8ef
lvfeng <lvfeng.org@gmail.com>
2024-06-16 21:08:35 +0800

    fix(渲染进程-BaseQueryDTO.ts): 去除getKeywordLikeString方法

--- src/renderer/src/model/main/queryDTO/BaseQueryDTO.ts
+++ src/renderer/src/model/main/queryDTO/BaseQueryDTO.ts
@@ -2,12 +2,13 @@ import BaseModel from '../BaseModel.ts'
 
 export default class BaseQueryDTO extends BaseModel {
   keyword: string | undefined | null
+
   constructor(baseQueryDTO?: BaseQueryDTO) {
     super(baseQueryDTO)
-    this.keyword = baseQueryDTO === undefined ? undefined : baseQueryDTO.keyword
-  }
-
-  public getKeywordLikeString() {
-    return '%' + this.keyword + '%'
+    if (baseQueryDTO === undefined) {
+      this.keyword = undefined
+    } else {
+      this.keyword = baseQueryDTO.keyword
+    }
   }
 }
