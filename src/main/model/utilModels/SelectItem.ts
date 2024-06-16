669babec297e37453441a5706106539009206c12
lvfeng <lvfeng.org@gmail.com>
2024-06-16 20:18:56 +0800

    perf(主进程-SelectItem.ts): 优化构造函数

--- src/main/model/utilModels/SelectItem.ts
+++ src/main/model/utilModels/SelectItem.ts
@@ -7,11 +7,20 @@ export default class SelectItem {
   secondaryLabel: string | null | undefined
   rootId: string | null | undefined
   extraData: object | null | undefined
-  constructor(selectItem: SelectItem) {
-    this.value = selectItem.value
-    this.label = selectItem.label
-    this.secondaryLabel = selectItem.secondaryLabel
-    this.rootId = selectItem.rootId
-    this.extraData = selectItem.extraData
+
+  constructor(selectItem?: SelectItem) {
+    if (selectItem === undefined) {
+      this.value = undefined
+      this.label = undefined
+      this.secondaryLabel = undefined
+      this.rootId = undefined
+      this.extraData = undefined
+    } else {
+      this.value = selectItem.value
+      this.label = selectItem.label
+      this.secondaryLabel = selectItem.secondaryLabel
+      this.rootId = selectItem.rootId
+      this.extraData = selectItem.extraData
+    }
   }
 }
