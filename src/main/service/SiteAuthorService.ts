0531d886b2ddd87e55747a4312d4ec3262b69685
lvfeng <lvfeng.org@gmail.com>
2024-06-23 02:03:26 +0800

    fix(主进程-SiteAuthorService.ts): 新增getBySiteAuthorId方法

--- src/main/service/SiteAuthorService.ts
+++ src/main/service/SiteAuthorService.ts
@@ -3,6 +3,8 @@ import SiteAuthor from '../model/SiteAuthor.ts'
 import BaseDao from '../dao/BaseDao.ts'
 import SiteAuthorQueryDTO from '../model/queryDTO/SiteAuthorQueryDTO.ts'
 import SiteAuthorDao from '../dao/SiteAuthorDao.ts'
+import StringUtil from '../util/StringUtil.ts'
+import LogUtil from '../util/LogUtil.ts'
 
 /**
  * 站点作者Service
@@ -10,6 +12,39 @@ import SiteAuthorDao from '../dao/SiteAuthorDao.ts'
 export default class SiteAuthorService extends BaseService<SiteAuthorQueryDTO, SiteAuthor> {
   constructor() {
     super('SiteAuthorService')
+  }
+
+  /**
+   * 根据站点作者id和站点id查询站点作者
+   * @param siteAuthorId
+   * @param siteId
+   */
+  public async getBySiteAuthorId(
+    siteAuthorId: string,
+    siteId: number
+  ): Promise<SiteAuthor | undefined> {
+    if (StringUtil.isNotBlank(siteAuthorId)) {
+      const dao = new SiteAuthorDao()
+
+      const queryDTO = new SiteAuthorQueryDTO()
+      queryDTO.siteAuthorId = siteAuthorId
+      queryDTO.siteId = siteId
+      queryDTO.sort = [{ column: 'createTime', order: 'desc' }]
+
+      const siteAuthors = await dao.selectList(queryDTO)
+      if (siteAuthors.length === 1) {
+        return siteAuthors[0]
+      }
+      if (siteAuthors.length > 1) {
+        LogUtil.warn('SiteAuthorService', `站点作者id：${siteAuthorId}在数据库中存在多个作者`)
+        return siteAuthors[0]
+      }
+      return undefined
+    } else {
+      const msg = '根据站点作者id查询站点作者时站点作者id意外为空'
+      LogUtil.error('SiteAuthorService', msg)
+      throw new Error(msg)
+    }
   }
 
   protected getDao(): BaseDao<SiteAuthorQueryDTO, SiteAuthor> {
