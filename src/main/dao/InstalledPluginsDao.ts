8ea73cd36c69454ce6a2cc5a57415e3fd7030ebf
zouwentao <lvfeng.org@gmail.com>
2024-06-28 17:28:58 +0800

    fix(主进程-InstalledPluginsDao.ts): 修复构造函数

--- src/main/dao/InstalledPluginsDao.ts
+++ src/main/dao/InstalledPluginsDao.ts
@@ -1,13 +1,14 @@
 import BaseDao from './BaseDao.ts'
 import InstalledPlugins from '../model/InstalledPlugins.ts'
 import InstalledPluginsQueryDTO from '../model/queryDTO/InstalledPluginsQueryDTO.ts'
+import DB from '../database/DB.ts'
 
 export default class InstalledPluginsDao extends BaseDao<
   InstalledPluginsQueryDTO,
   InstalledPlugins
 > {
-  constructor() {
-    super('installed_plugins', 'InstalledPluginsDao')
+  constructor(db?: DB) {
+    super('installed_plugins', 'InstalledPluginsDao', db)
   }
   protected getPrimaryKeyColumnName(): string {
     return 'id'
