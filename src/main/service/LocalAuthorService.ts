bacd429bad9177d131e3da4fec76a9e8be6f6ae1
lvfeng <lvfeng.org@gmail.com>
2024-07-01 00:15:25 +0800

    fix(主进程-LocalAuthorService.ts): link方法调整为处理多个本地作者

--- src/main/service/LocalAuthorService.ts
+++ src/main/service/LocalAuthorService.ts
@@ -9,9 +9,9 @@ import PageModel from '../model/utilModels/PageModel.ts'
 import DB from '../database/DB.ts'
 import WorksDTO from '../model/dto/WorksDTO.ts'
 import ReWorksAuthor from '../model/ReWorksAuthor.ts'
-import { AuthorRole } from '../constant/AuthorRole.ts'
 import { ReWorksAuthorTypeEnum } from '../constant/ReWorksAuthorTypeEnum.ts'
 import ReWorksAuthorService from './ReWorksAuthorService.ts'
+import LocalAuthorDTO from '../model/dto/LocalAuthorDTO.ts'
 
 /**
  * 本地作者Service
@@ -23,16 +23,18 @@ export default class LocalAuthorService extends BaseService<LocalAuthorQueryDTO,
 
   /**
    * 关联本地作者和作品
-   * @param localAuthor 作者信息
+   * @param localAuthorDTOs 作者信息
    * @param worksDTO 作品信息
-   * @param authorRole 作者角色
    */
-  async link(localAuthor: LocalAuthor, worksDTO: WorksDTO, authorRole: AuthorRole) {
-    const reWorksAuthor = new ReWorksAuthor()
-    reWorksAuthor.worksId = worksDTO.id as number
-    reWorksAuthor.authorRole = authorRole
-    reWorksAuthor.localAuthorId = localAuthor.id as number
-    reWorksAuthor.type = ReWorksAuthorTypeEnum.LOCAL
+  async link(localAuthorDTOs: LocalAuthorDTO[], worksDTO: WorksDTO) {
+    const reWorksAuthors = localAuthorDTOs.map((localAuthorDTO) => {
+      const reWorksAuthor = new ReWorksAuthor()
+      reWorksAuthor.worksId = worksDTO.id as number
+      reWorksAuthor.authorRole = localAuthorDTO.authorRole
+      reWorksAuthor.localAuthorId = localAuthorDTO.id as number
+      reWorksAuthor.type = ReWorksAuthorTypeEnum.LOCAL
+      return reWorksAuthors
+    })
 
     // 调用ReWorksAuthorService前区分是否为注入式DB
     let reWorksAuthorService: ReWorksAuthorService
@@ -42,7 +44,7 @@ export default class LocalAuthorService extends BaseService<LocalAuthorQueryDTO,
       reWorksAuthorService = new ReWorksAuthorService()
     }
 
-    return reWorksAuthorService.save(reWorksAuthor)
+    return reWorksAuthorService.saveBatch(reWorksAuthors, true)
   }
 
   /**
