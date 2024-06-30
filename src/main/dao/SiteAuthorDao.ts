ff6c678f51f27595f0811efb2a57cfa5beca98fd
lvfeng <lvfeng.org@gmail.com>
2024-07-01 00:15:46 +0800

    fix(主进程-SiteAuthorDao.ts): 新增selectListBySiteAuthorIds方法

--- src/main/dao/SiteAuthorDao.ts
+++ src/main/dao/SiteAuthorDao.ts
@@ -11,6 +11,21 @@ export default class SiteAuthorDao extends BaseDao<SiteAuthorQueryDTO, SiteAutho
     super('site_author', 'SiteAuthorDao', db)
   }
 
+  /**
+   * 根据站点作者Id列表查询
+   * @param siteAuthorIs
+   */
+  async selectListBySiteAuthorIds(siteAuthorIs: string[]) {
+    const db = this.acquire()
+    try {
+      const statement = `SELECT * FROM "${this.tableName}" WHERE site_author_id in ${siteAuthorIs.join(',')}`
+      const rows = (await db.prepare(statement)).all() as object[]
+      return this.getResultTypeDataList<SiteAuthor>(rows)
+    } finally {
+      db.release()
+    }
+  }
+
   protected getPrimaryKeyColumnName(): string {
     return 'id'
   }
