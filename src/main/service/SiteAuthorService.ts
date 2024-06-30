bd4fbe66ab0f496ebd00a6e498705e85cf8ce6e6
lvfeng <lvfeng.org@gmail.com>
2024-07-01 00:16:12 +0800

    fix(主进程-SiteAuthorService.ts): 新增saveOrUpdateBySiteAuthorId和saveOrUpdateBatchBySiteAuthorId方法

--- src/main/service/SiteAuthorService.ts
+++ src/main/service/SiteAuthorService.ts
@@ -16,39 +16,6 @@ export default class SiteAuthorService extends BaseService<SiteAuthorQueryDTO, S
   }
 
   /**
-   * 根据站点作者id和站点id查询站点作者
-   * @param siteAuthorId
-   * @param siteId
-   */
-  public async getBySiteAuthorId(
-    siteAuthorId: string,
-    siteId: number
-  ): Promise<SiteAuthor | undefined> {
-    if (StringUtil.isNotBlank(siteAuthorId)) {
-      const dao = new SiteAuthorDao()
-
-      const queryDTO = new SiteAuthorQueryDTO()
-      queryDTO.siteAuthorId = siteAuthorId
-      queryDTO.siteId = siteId
-      queryDTO.sort = [{ column: 'createTime', order: 'desc' }]
-
-      const siteAuthors = await dao.selectList(queryDTO)
-      if (siteAuthors.length === 1) {
-        return siteAuthors[0]
-      }
-      if (siteAuthors.length > 1) {
-        LogUtil.warn('SiteAuthorService', `站点作者id：${siteAuthorId}在数据库中存在多个作者`)
-        return siteAuthors[0]
-      }
-      return undefined
-    } else {
-      const msg = '根据站点作者id查询站点作者时站点作者id意外为空'
-      LogUtil.error('SiteAuthorService', msg)
-      throw new Error(msg)
-    }
-  }
-
-  /**
    * 基于站点作者id保存或更新站点作者
    * @param siteAuthor
    */
@@ -84,6 +51,82 @@ export default class SiteAuthorService extends BaseService<SiteAuthorQueryDTO, S
         await this.save(newSiteAuthor)
         return 1
       }
+    }
+  }
+
+  /**
+   * 基于站点作者id批量保存或更新站点作者
+   * @param siteAuthors
+   */
+  public async saveOrUpdateBatchBySiteAuthorId(siteAuthors: SiteAuthor[]): Promise<number> {
+    const dao = new SiteAuthorDao()
+    const siteAuthorIds = siteAuthors.map((siteAuthor) => siteAuthor.siteAuthorId) as string[]
+    const oldSiteAuthors = await dao.selectListBySiteAuthorIds(siteAuthorIds)
+    const newSiteAuthors: SiteAuthor[] = []
+    siteAuthors.forEach((siteAuthor) => {
+      if (siteAuthor.siteAuthorId === undefined || siteAuthor.siteAuthorId === null) {
+        const msg = '保存作品时，站点作者的id意外为空'
+        LogUtil.error('SiteAuthorService', '保存作品时，站点作者的id意外为空')
+        throw new Error(msg)
+      } else if (siteAuthor.siteId === undefined || siteAuthor.siteId === null) {
+        newSiteAuthors.push(siteAuthor)
+      } else {
+        const oldSiteAuthor = oldSiteAuthors.find(
+          (oldSiteAuthor) => oldSiteAuthor.siteAuthorId === siteAuthor.siteAuthorId
+        )
+        const newSiteAuthor = lodash.cloneDeep(siteAuthor)
+
+        if (oldSiteAuthor !== undefined) {
+          // 调整新数据
+          newSiteAuthor.id = oldSiteAuthor.id
+          newSiteAuthor.siteAuthorNameBefore = oldSiteAuthor.siteAuthorNameBefore
+          newSiteAuthor.createTime = undefined
+          newSiteAuthor.updateTime = undefined
+          // 如果站点作者的名称变更，对原本的名称写入到siteAuthorNameBefore
+          if (
+            newSiteAuthor.siteAuthorName !== oldSiteAuthor.siteAuthorName &&
+            oldSiteAuthor.siteAuthorName !== undefined &&
+            oldSiteAuthor.siteAuthorName !== null
+          ) {
+            ;(newSiteAuthor.siteAuthorNameBefore as string[]).push(oldSiteAuthor.siteAuthorName)
+          }
+          newSiteAuthors.push(newSiteAuthor)
+        }
+      }
+    })
+    return super.saveOrUpdateBatchById(newSiteAuthors)
+  }
+
+  /**
+   * 根据站点作者id和站点id查询站点作者
+   * @param siteAuthorId
+   * @param siteId
+   */
+  public async getBySiteAuthorId(
+    siteAuthorId: string,
+    siteId: number
+  ): Promise<SiteAuthor | undefined> {
+    if (StringUtil.isNotBlank(siteAuthorId)) {
+      const dao = new SiteAuthorDao()
+
+      const queryDTO = new SiteAuthorQueryDTO()
+      queryDTO.siteAuthorId = siteAuthorId
+      queryDTO.siteId = siteId
+      queryDTO.sort = [{ column: 'createTime', order: 'desc' }]
+
+      const siteAuthors = await dao.selectList(queryDTO)
+      if (siteAuthors.length === 1) {
+        return siteAuthors[0]
+      }
+      if (siteAuthors.length > 1) {
+        LogUtil.warn('SiteAuthorService', `站点作者id：${siteAuthorId}在数据库中存在多个作者`)
+        return siteAuthors[0]
+      }
+      return undefined
+    } else {
+      const msg = '根据站点作者id查询站点作者时站点作者id意外为空'
+      LogUtil.error('SiteAuthorService', msg)
+      throw new Error(msg)
     }
   }
 }
