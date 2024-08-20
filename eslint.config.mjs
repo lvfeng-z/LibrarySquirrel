69ffdb57d54f7a23ac25968df2a60464e5ee9f14
lvfeng <lvfeng.org@gmail.com>
2024-08-20 11:01:03 +0800

    build: 升级依赖

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+
+export default [
+  {
+    ignores: ['**/node_modules', '**/dist', '**/out', '**/.gitignore']
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@electron-toolkit',
+    '@electron-toolkit/eslint-config-ts/eslint-recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier'
+  ),
+  {
+    rules: {
+      'vue/require-default-prop': 'off',
+      'vue/multi-word-component-names': 'off'
+    }
+  }
+]
