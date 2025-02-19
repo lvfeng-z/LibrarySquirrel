0a6279be2c86b244cb8f303c8eb3ea66b2352152
lvfeng <lvfeng.org@gmail.com>
2025-02-19 10:30:25 +0800

    build: 升级依赖

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,26 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import prettierConfig from '@vue/eslint-config-prettier'
+import tsEslint from '@electron-toolkit/eslint-config-ts'
+import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
+import eslintPluginVue from 'eslint-plugin-vue'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-})
-
-export default [
+export default tsEslint.config(
+  { ignores: ['**/node_modules', '**/dist', '**/out'] },
+  tsEslint.configs.recommended,
+  eslintPluginVue.configs['flat/recommended'],
   {
-    ignores: ['**/node_modules', '**/dist', '**/out', '**/.gitignore', '**/resources/plugins/**/code/**_product.mjs']
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tsEslint.parser
+      }
+    }
   },
-  ...compat.extends('eslint:recommended', '@electron-toolkit/eslint-config-ts/eslint-recommended'),
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  prettierConfig,
   {
+    files: ['**/*.{ts,mts,tsx,vue}'],
     rules: {
       'vue/require-default-prop': 'off',
       'vue/multi-word-component-names': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off'
     }
-  }
-]
+  },
+  eslintConfigPrettier
+)
