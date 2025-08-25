d8e27a2b787551367a717c02b923718fd4b34086
lvfeng <lvfeng.org@gmail.com>
2025-08-26 00:44:59 +0800

    fix(主进程-插件): 修复获取监听url的插件方法的sql错误

--- src/main/dao/PluginTaskUrlListenerDao.ts
+++ src/main/dao/PluginTaskUrlListenerDao.ts
@@ -15,7 +15,7 @@ export default class PluginTaskUrlListenerDao extends BaseDao<PluginTaskUrlListe
    */
   async listListener(url: string): Promise<Plugin[]> {
     const db = this.acquire()
-    const statement = `SELECT DISTINCT t1.* FROM plugin t1 INNER JOIN task_plugin_listener t2 ON t1.id = t2.plugin_id WHERE @url REGEXP t2.listener ORDER BY t1.sort_num`
+    const statement = `SELECT DISTINCT t1.* FROM plugin t1 INNER JOIN ${this.tableName} t2 ON t1.id = t2.plugin_id WHERE @url REGEXP t2.listener ORDER BY t1.sort_num`
     return db
       .all<unknown[], Record<string, unknown>>(statement, { url: url })
       .then((result) => this.toResultTypeDataList<Plugin>(result))
