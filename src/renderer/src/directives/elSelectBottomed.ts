1300166dbcf018a3fadab6cce84af29931be6f56
lvfeng <lvfeng.org@gmail.com>
2025-08-29 11:19:19 +0800

    fix(渲染进程): 修复el-select的自定义触底指令在内容变少时会触发的问题

--- src/renderer/src/directives/elSelectBottomed.ts
+++ src/renderer/src/directives/elSelectBottomed.ts
@@ -6,12 +6,19 @@ export default {
       const scrollTop = domTarget.scrollTop
       const clientHeight = domTarget.clientHeight
       const scrollHeight = domTarget.scrollHeight
+      // 内容没满一屏就不触发，可防止内容变少导致的触底
+      if (scrollHeight <= clientHeight) {
+        return
+      }
       if (scrollHeight - scrollTop <= clientHeight) {
+        // 把input元素的value属性（即输入的文本）传递过去
         binding.value(el.firstElementChild.firstElementChild.firstElementChild.firstElementChild.value)
       }
     }
+    // 获取 el-select__input 携带的 aria-controls 属性
     const child = el.querySelector('.el-select__input')
     const id = child.getAttribute('aria-controls')
+    // 通过 aria-controls 找到 popper 元素
     const popper = document.getElementById(id)
     if (popper != null) {
       const selectWrapper = popper.parentElement
